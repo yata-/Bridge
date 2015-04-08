89c8c32f4102dbd2517d81c6e66cb7b796e2aed7
Geoffrey McGill <geoff@object.net>
2015-04-08 16:49:06 -0600

    Added quotes to paths to enable spaces within pre-build and .bat paths. Closes #161

--- Bridge/Resources/build.bat
+++ Bridge/Resources/build.bat
@@ -1,4 +1,4 @@
-%~dp0\JSBuilder\JSBuildConsole.exe /path=%~dp0\Bridge.jsb
+"%~dp0\JSBuilder\JSBuildConsole.exe" /path="%~dp0\Bridge.jsb"
 
-move /y %~dp0\bridge.js %~dp0\bridge.min.js
-move /y %~dp0\bridge-debug.js %~dp0\bridge.js
+move /y "%~dp0\bridge.js" "%~dp0\bridge.min.js"
+move /y "%~dp0\bridge-debug.js" "%~dp0\bridge.js"
