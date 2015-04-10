607e6ed45e9ad55b858be3ca0c2f727ce19cd9ab
Geoffrey McGill <geoff@object.net>
2015-04-09 23:57:28 -0600

    Update to version 1.2

--- Build/Properties/AssemblyInfo.cs
+++ Build/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("736cfee7-d77d-499a-9da0-d95efbb07fdf")]
-[assembly: AssemblyVersion("1.1")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyFileVersion("1.2.0")]
