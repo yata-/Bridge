84ffaf69203fe1bb96ff9b1f6a44d46694aad083
Geoffrey McGill <geoff@object.net>
2015-06-23 00:01:50 -0600

    Update to 1.6.0 release

--- Html5/Properties/AssemblyInfo.cs
+++ Html5/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d5956119-f924-47e1-96c2-4d98b832eb38")]
-[assembly: AssemblyVersion("1.5")]
-[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6")]
+[assembly: AssemblyFileVersion("1.6.0")]
