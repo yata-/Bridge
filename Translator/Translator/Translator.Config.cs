40f92fd9af48f196c94977a2a6869e7d7fb5d5e9
LeonidVeriga <leonid@object.com>
2015-06-25 22:42:40 +0300

    ConfigHelper into Bridge.Contract

--- Translator/Translator/Translator.Config.cs
+++ Translator/Translator/Translator.Config.cs
@@ -1,91 +1,21 @@
 ﻿using System;
 using System.Diagnostics;
-using Bridge.Contract;
 using System.IO;
+
+using Bridge.Contract;
+using Bridge.Translator.Utils;
+
 using Newtonsoft.Json;
 
 namespace Bridge.Translator
 {
     public partial class Translator
     {
-        private void ConvertConfigPaths(IAssemblyInfo assemblyInfo)
-        {
-            if (!string.IsNullOrWhiteSpace(assemblyInfo.AfterBuild))
-            {
-                assemblyInfo.AfterBuild = ConvertPath(assemblyInfo.AfterBuild);
-            }
-
-            if (!string.IsNullOrWhiteSpace(assemblyInfo.BeforeBuild))
-            {
-                assemblyInfo.BeforeBuild = ConvertPath(assemblyInfo.BeforeBuild);
-            }
-
-            if (!string.IsNullOrWhiteSpace(assemblyInfo.Output))
-            {
-                assemblyInfo.Output = ConvertPath(assemblyInfo.Output);
-            }
-
-            if (!string.IsNullOrWhiteSpace(assemblyInfo.PluginsPath))
-            {
-                assemblyInfo.PluginsPath = ConvertPath(assemblyInfo.PluginsPath);
-            }
-        }
-
-        private string ConvertPath(string path)
-        {
-            if (Path.DirectorySeparatorChar != '/')
-            {
-                path = path.Replace('/', Path.DirectorySeparatorChar);
-            }
-
-            if (Path.DirectorySeparatorChar != '\\')
-            {
-                path.Replace('\\', Path.DirectorySeparatorChar);
-            }
-
-            return path;
-        }
-
         protected virtual IAssemblyInfo ReadConfig()
         {
-            var folder = this.FolderMode ? this.Location : Path.GetDirectoryName(this.Location);
-            var path = folder + Path.DirectorySeparatorChar + "bridge" + Path.DirectorySeparatorChar + "bridge.json";
+            var config = AssemblyConfigHelper.ReadConfig(this.FolderMode, this.Location);
 
-            if (!File.Exists(path))
-            {
-                path = folder + Path.DirectorySeparatorChar + "bridge.json";
-            }
-
-            if (!File.Exists(path))
-            {
-                path = folder + Path.DirectorySeparatorChar + "Bridge.NET" + Path.DirectorySeparatorChar + "bridge.json";
-            }
-
-            if (!File.Exists(path))
-            {
-                var config = new AssemblyInfo();
-                return config;
-            }
-
-            try
-            {
-                var json = File.ReadAllText(path);
-                IAssemblyInfo assemblyInfo = JsonConvert.DeserializeObject<AssemblyInfo>(json);
-
-                if (assemblyInfo == null)
-                {
-                    assemblyInfo = new AssemblyInfo();
-                }
-
-                // Convert '/' and '\\' to platform-specific path separator.
-                ConvertConfigPaths(assemblyInfo);
-
-                return assemblyInfo;
-            }
-            catch (Exception e)
-            {
-                throw new InvalidOperationException("Cannot read bridge.json", e);
-            }
+            return config;
         }
 
         public virtual void RunEvent(string e)
