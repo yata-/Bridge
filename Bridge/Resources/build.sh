260feca3b47d76eebcd802f0a874a83ddc23014c
Fabrício Zimmerer Murta <fabricio@object.net>
2015-07-08 16:36:55 -0300

    Makes script changes compatible with OS X. [#288]

--- Bridge/Resources/build.sh
+++ Bridge/Resources/build.sh
@@ -26,11 +26,32 @@ if [ ! -e "${jsbfile}" ]; then
  exit 1
 fi
 
+# In order to try to preserve a single sed command, will use extended regexp
+# which is triggered with '-r' on GNU sed and '-E' on OS X.
+# Extended regexp is more similar between the two sed versions.
+if [ "$(uname -s)" == "Darwin" ]; then
+ osx=true
+ sedopts="-E" # sed's option to extended regexp
+
+ # sed from OS X needs escape chars specified this way in order to work.
+ sednl="\\
+" # 'echo -ne' trick does not work for newline
+ sedBOM="$(echo -ne "\xef\xbb\xbf")"
+else
+ osx=false
+ sedopts="-r" # sed's option to extended regexp
+
+ sednl="\n"
+ sedBOM="\xef\xbb\xbf"
+fi
+
+sedcmd="sed ${sedopts}"
+
 fileheader="/*
- * $(grep 'copyright *=' "${jsbfile}" | head -n1 | sed "s/.*copyright *= *\"\([^\"]\+\)\".*/\1/g;s/&#xD;&#xA;/\n * /g")
+ * $(grep 'copyright *=' "${jsbfile}" | head -n1 | ${sedcmd} "s/.*copyright *= *\"([^\"]+)\".*/\1/g;s/&#xD;&#xA;/${sednl} * /g")
  */"
 
-filelist="$(cat "${jsbfile}" | grep "<include name=" | sed "s/^.*<include name=\"\(.*\)\" *\/>.*\$/\1/")"
+filelist="$(cat "${jsbfile}" | grep "<include name=" | ${sedcmd} "s/^.*<include name=\"(.*)\" *\/>.*\$/\1/")"
 
 echo "Merging $(echo "${filelist}" | wc -l) files."
 if [ -e "${tmpbrjs}" ]; then
@@ -51,7 +72,7 @@ for file in ${filelist}; do
 #  echo "
 #// @source ${file}
 #" >> "${tmpbrjs}"
-  sed "1s/^\xef\xbb\xbf//" "${file}" >> "${tmpbrjs}"
+  ${sedcmd} "1s/^${sedBOM}//" "${file}" >> "${tmpbrjs}"
   echo "" >> "${tmpbrjs}" # add EOL at EOF. :)
  else
   echo "File '${file}' not found."
@@ -65,14 +86,14 @@ function minify() {
  cp "${tmpbrjs}" "${jsminf}"
 
  echo "Minifying '${jsminf}'."
- sedcmd="s#(^|^.*[^:])//.*\$#\1#g;s/^ *//g;s/ *\$//g;s/  +/ /g;/^ *\$/d"
- if [ "$(uname -s)" == "Darwin" ]; then
-  sed -i '' -E -e "s/	/ /g" "${jsminf}" # Replate tab chars
-  LC_ALL=C sed -i '' -E -e "${sedcmd}" "${jsminf}"
+ sedscp="s#(^|^.*[^:])//.*\$#\1#g;s/^ *//g;s/ *\$//g;s/  +/ /g;/^ *\$/d"
+ if ${osx}; then
+  ${sedcmd} -i '' -e "s/$(echo -ne "\t")/ /g" "${jsminf}" # Replate tab chars
+  LC_ALL=C ${sedcmd} -i '' -e "${sedscp}" "${jsminf}"
  else
   # TODO: Check on linux!
-  sed -i -E -e "s/\t/ /g" "${jsminf}" # Replace tab chars
-  sed -i -E -e "${sedcmd}" "${jsminf}"
+  ${sedcmd} -i -e "s/\t/ /g" "${jsminf}" # Replace tab chars
+  ${sedcmd} -i -e "${sedscp}" "${jsminf}"
  fi
 
  # Removing line breaks from JavaScript files can break code in some situations.
