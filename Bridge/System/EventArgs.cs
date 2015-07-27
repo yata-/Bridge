eeed2dbfaed850f6251b0c7db1d49021b3f4478a
Fabrício Zimmerer Murta <fabricio@object.net>
2015-07-27 03:15:27 -0300

    Create EventArgs class. [#326]

---
+++ Bridge/System/EventArgs.cs
@@ -0,0 +1,22 @@
+﻿using Bridge;
+using System;
+
+namespace System
+{
+    // Summary:
+    //     System.EventArgs is the base class for classes containing event data.
+    [Ignore]
+    [Name("Object")]
+    public class EventArgs
+    {
+        // Summary:
+        //     Represents an event with no event data.
+        public static readonly EventArgs Empty;
+
+        // Summary:
+        //     Initializes a new instance of the System.EventArgs class.
+        public EventArgs()
+        {
+        }
+    }
+}
