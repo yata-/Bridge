f6aae10730b770315afb062861a2568911fef58b
Fabrício Zimmerer Murta <fabricio@object.net>
2015-07-27 03:22:38 -0300

    Reviews System.EventArgs class' comments. [#326]

--- Bridge/System/EventArgs.cs
+++ Bridge/System/EventArgs.cs
@@ -3,18 +3,21 @@ using System;
 
 namespace System
 {
-    // Summary:
-    //     System.EventArgs is the base class for classes containing event data.
+    /// <summary>
+    /// System.EventArgs is the base class for classes containing event data.
+    /// </summary>
     [Ignore]
     [Name("Object")]
     public class EventArgs
     {
-        // Summary:
-        //     Represents an event with no event data.
+        /// <summary>
+        /// Represents an event with no event data.
+        /// </summary>
         public static readonly EventArgs Empty;
 
-        // Summary:
-        //     Initializes a new instance of the System.EventArgs class.
+        /// <summary>
+        /// Initializes a new instance of the System.EventArgs class.
+        /// </summary>
         public EventArgs()
         {
         }
