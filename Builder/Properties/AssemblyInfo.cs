cc12f1b3bdd35833b28cdc4a7807a1c2bb16d1ae
Geoffrey McGill <geoff@object.net>
2015-08-08 08:12:08 -0600

    Update to Bridge version 1.8.0

--- Builder/Properties/AssemblyInfo.cs
+++ Builder/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("44bd50e3-3b1b-4c13-90fa-8ab048f2c0c0")]
-[assembly: AssemblyVersion("1.7")]
-[assembly: AssemblyFileVersion("1.7.0")]
+[assembly: AssemblyVersion("1.8")]
+[assembly: AssemblyFileVersion("1.8.0")]
--- Translator/Properties/AssemblyInfo.cs
+++ Translator/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ac6f7c4d-87e1-4f66-912b-6fe33383543c")]
-[assembly: AssemblyVersion("1.7")]
-[assembly: AssemblyFileVersion("1.7.0")]
+[assembly: AssemblyVersion("1.8")]
+[assembly: AssemblyFileVersion("1.8.0")]
