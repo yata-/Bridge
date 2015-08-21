9378d7f043c9876b7a07bfcf48c95c23fa01fb13
Fabrício Zimmerer Murta <fabricio@object.net>
2015-08-21 11:21:41 -0300

    [ADD] NativeMethod attribute to render extension method invocation as native method

---
+++ Bridge/Attributes/NativeMethodAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Bridge
+{
+    [Ignore]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class NativeMethodAttribute : Attribute
+    {
+    }
+}
