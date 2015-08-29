6dc03daeb0e86ed42f49bb94c92eb933e1b2e127
Fabrício Zimmerer Murta <fabricio@object.net>
2015-08-29 18:16:58 -0300

    Force-add .ts and .d.ts files from Testing project. [#405]

---
+++ Testing/www/typescript/classes.d.ts
@@ -0,0 +1,84 @@
+﻿/// <reference path="./bridge.d.ts" />
+
+declare module Classes {
+    export interface MovePoint {
+        getPoint(): Classes.Point;
+        setPoint(value: Classes.Point): void;
+        move(dx: number, dy: number): void;
+    }
+    export interface MovePointFunc extends Function {
+        prototype: MovePoint;
+        new (): MovePoint;
+        move(p: Classes.Point, dx: number, dy: number): Classes.Point;
+    }
+    var MovePoint: MovePointFunc;
+
+    export interface Point {
+        x: number;
+        y: number;
+        getHashCode(): Classes.Point;
+        equals(o: Classes.Point): Boolean;
+        $clone(to: Classes.Point): Classes.Point;
+    }
+    export interface PointFunc extends Function {
+        prototype: Point;
+        constructor$1: {
+            new (x: number, y: number): Point
+        };
+        $constructor: {
+            new (): Point
+        };
+    }
+    var Point: PointFunc;
+
+    export interface StaticClass {
+    }
+    export interface StaticClassFunc extends Function {
+        prototype: StaticClass;
+        new (): StaticClass;
+        move(p: Classes.Point, dx: number, dy: number): Classes.Point;
+    }
+    var StaticClass: StaticClassFunc;
+
+    export interface Dog extends Classes.Animal {
+        move$1(): number;
+    }
+    export interface DogFunc extends Function {
+        prototype: Dog;
+        new (name: string): Dog;
+    }
+    var Dog: DogFunc;
+
+    export interface Animal {
+        getName(): string;
+        move(): number;
+    }
+    export interface AnimalFunc extends Function {
+        prototype: Animal;
+        $constructor: {
+            new (): Animal
+        };
+        constructor$1: {
+            new (name: string): Animal
+        };
+    }
+    var Animal: AnimalFunc;
+
+    export interface Snake extends Classes.Animal {
+        move(): number;
+    }
+    export interface SnakeFunc extends Function {
+        prototype: Snake;
+        new (name: string): Snake;
+    }
+    var Snake: SnakeFunc;
+
+    export interface Employee extends Classes.Animal {
+    }
+    export interface EmployeeFunc extends Function {
+        prototype: Employee;
+        new (name: string, id: number): Employee;
+    }
+    var Employee: EmployeeFunc;
+
+}
