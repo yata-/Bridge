41a68bb2bc6bf0e9475a52025b494fd6cb276d34
LeonidVeriga <leonid@object.net>
2015-09-15 22:05:22 +0300

    [AppVeyor] Normalize EOL to LF for js [skip ci]

--- Compiler/TranslatorTests/TestProjects/18/Bridge/reference/test1.js
+++
@@ -1,7 +0,0 @@
-﻿/* global Bridge */
-
-"use strict";
-Bridge.define('Test1.A1');
-
-Bridge.define('Test1.B1');
-
--- Compiler/TranslatorTests/TestProjects/18/Bridge/reference/test2.js
+++
@@ -1,7 +0,0 @@
-﻿/* global Bridge */
-
-"use strict";
-Bridge.define('Test2.A2');
-
-Bridge.define('Test2.B2');
-
