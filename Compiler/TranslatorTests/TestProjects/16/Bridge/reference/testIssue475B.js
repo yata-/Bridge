935466413e36fce6bf51793fd5243b85cc41dbf9
LeonidVeriga <leonid@object.net>
2015-09-18 16:12:29 +0300

    [#475] [TEST]

---
+++ Compiler/TranslatorTests/TestProjects/16/Bridge/reference/testIssue475B.js
@@ -0,0 +1,29 @@
+﻿/* global Bridge */
+
+"use strict";
+Bridge.define('TestIssue475B.Bridge475Event', {
+    config: {
+        properties: {
+            Data: 0
+        }
+    },
+    preventDefault: function () {
+        this.setData(77);
+    }
+});
+
+Bridge.define('TestIssue475B.Test', {
+    statics: {
+        n475: function () {
+            var b = new TestIssue475B.Bridge475();
+
+            b.keyDown(function (ev) {
+                ev.preventDefault();
+            });
+
+            b.keyDown(4);
+
+            b.keyDown("5");
+        }
+    }
+});
