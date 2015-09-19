18cba05051cf6085fbc485b7442b273281fd0c87
LeonidVeriga <leonid@object.net>
2015-09-19 16:33:11 +0300

    [#472] [TEST]

--- Testing/www/js/testRunner.js
+++ Testing/www/js/testRunner.js
@@ -104,6 +104,7 @@ Bridge.define('ClientTestLibrary.RunTests', {
             QUnit.test("#467", ClientTestLibrary.TestBridgeIssues.n467);
             QUnit.test("#469", ClientTestLibrary.TestBridgeIssues.n469);
             QUnit.test("#470", ClientTestLibrary.TestBridgeIssues.n470);
+            QUnit.test("#472", ClientTestLibrary.Bridge472.test);
 
             QUnit.module("LINQ");
             QUnit.test("Aggregate operators", ClientTestLibrary.Linq.TestLinqAggregateOperators.test);
