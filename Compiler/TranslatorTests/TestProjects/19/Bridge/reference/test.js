76ad5fe378bafde31cc1089956118f4beb2fe6d2
vladsch <vladimir@object.net>
2015-11-12 03:00:27 +0300

    [TEST] [#599] Update unit tests for #599

---
+++ Compiler/TranslatorTests/TestProjects/19/Bridge/reference/test.js
@@ -0,0 +1,25 @@
+﻿/* global Bridge */
+
+"use strict";
+
+Bridge.define('Test.App', {
+    statics: {
+        config: {
+            init: function () {
+                Bridge.ready(this.main);
+            }
+        },
+        main: function () {
+            var inst = new Test.App();
+        }
+    },
+    _something: "HI!",
+    config: {
+        init: function () {
+            Bridge.ready(this.test, this);
+        }
+    },
+    test: function () {
+        console.log(this._something);
+    }
+});
---
+++ Compiler/TranslatorTests/TestProjects/19/CheckReadyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Bridge.Html5;
+
+namespace Test
+{
+    public class App
+    {
+        private string _something = "HI!";
+
+        [Ready]
+        public static void Main()
+        {
+            var inst = new App();
+        }
+
+        [Ready]
+        public void Test()
+        {
+            Console.WriteLine(_something);
+        }
+    }
+}
