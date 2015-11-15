c73da2465e16eec5046881e44ff4814842a46aff
LeonidVeriga <leonid@object.net>
2015-11-15 14:55:14 +0300

    [TEST] [#580] added test for long overload

--- Testing/tests/client/Tests/BridgeIssues/N580.cs
+++ Testing/tests/client/Tests/BridgeIssues/N580.cs
@@ -13,21 +13,43 @@ namespace ClientTestLibrary
     {
         public static void TestUseCase(Assert assert)
         {
-            assert.Expect(5);
+            assert.Expect(10);
 
             string[] arrs = new string[] { "s1", "s2" };
-            string[] dst = new string[2];
-            arrs.CopyTo(dst, 0);
 
-            assert.Equal(dst.Length, 2, "Bridge580 Length");
-            assert.Equal(dst[0], arrs[0], "Bridge580 0");
-            assert.Equal(dst[1], arrs[1], "Bridge580 1");
+            int intIndex;
+
+            string[] dst = new string[2];
+            intIndex = 0;
+            arrs.CopyTo(dst, intIndex);
+
+            assert.Equal(dst.Length, 2, "Bridge580 Length Int");
+            assert.Equal(dst[0], arrs[0], "Bridge580 0 Int");
+            assert.Equal(dst[1], arrs[1], "Bridge580 1 Int");
 
             dst = new string[1];
-            arrs.CopyTo(dst, 1);
+            intIndex = 1;
+            arrs.CopyTo(dst, intIndex);
 
-            assert.Equal(dst.Length, 1, "Bridge580 Length 1");
-            assert.Equal(dst[0], arrs[1], "Bridge580 1_1");
+            assert.Equal(dst.Length, 1, "Bridge580 Length 1 Int");
+            assert.Equal(dst[0], arrs[1], "Bridge580 1_1 Int");
+
+            long longIndex;
+
+            dst = new string[2];
+            longIndex = 0;
+            arrs.CopyTo(dst, longIndex);
+
+            assert.Equal(dst.Length, 2, "Bridge580 Length Long");
+            assert.Equal(dst[0], arrs[0], "Bridge580 0 Long");
+            assert.Equal(dst[1], arrs[1], "Bridge580 1 Long");
+
+            dst = new string[1];
+            longIndex = 1;
+            arrs.CopyTo(dst, longIndex);
+
+            assert.Equal(dst.Length, 1, "Bridge580 Length 1 Long");
+            assert.Equal(dst[0], arrs[1], "Bridge580 1_1 Long");
         }
     }
 }
