5ba1e1d261a1e0835501a3d93d6305a1b280b0bf
DaniilVeriga <daniil@object.net>
2015-11-16 15:39:01 +0300

    [FIX] [#550] Turned ArrayBufferView to be an alias of TypeArrays and DataView.

--- Html5/TypedArray/ArrayBufferView.cs
+++ Html5/TypedArray/ArrayBufferView.cs
@@ -1,30 +1,127 @@
 namespace Bridge.Html5
 {
     /// <summary>
-    /// The ArrayBufferView type describes a particular view on the contents of an ArrayBuffer's data.
-    /// Of note is that you may create multiple views into the same buffer, each looking at the buffer's contents starting at a particular offset. This makes it possible to set up views of different data types to read the contents of a buffer based on the types of data at specific offsets into the buffer.
+    /// ArrayBufferView is a helper type representing any of the following JavaScript TypedArray types:
+    /// 
+    ///     Int8Array,
+    ///     Uint8Array,
+    ///     Uint8ClampedArray,
+    ///     Int16Array,
+    ///     Uint16Array,
+    ///     Int32Array,
+    ///     Uint32Array,
+    ///     Float32Array,
+    ///     Float64Array or
+    ///     DataView.
+    ///
+    /// This is a helper type to simplify the specification, it isn't an interface and there are no objects implementing it.
     /// </summary>
     [Ignore]
-    [Name("ArrayBufferView")]
+    [IgnoreGeneric]
+    [IgnoreCast]
+    [Name("Object")]
     public class ArrayBufferView
     {
-        protected ArrayBufferView()
+        public static implicit operator ArrayBufferView(Int8Array t)
         {
+            return null;
         }
 
-        /// <summary>
-        /// The buffer this view references. Read only.
-        /// </summary>
-        public readonly ArrayBuffer Buffer;
+        public static implicit operator ArrayBufferView(Uint8Array t)
+        {
+            return null;
+        }
 
-        /// <summary>
-        /// The length, in bytes, of the view. Read only.
-        /// </summary>
-        public readonly long ByteLength;
+        public static implicit operator ArrayBufferView(Uint8ClampedArray t)
+        {
+            return null;
+        }
 
-        /// <summary>
-        /// The offset, in bytes, to the first byte of the view within the ArrayBuffer. Read only.
-        /// </summary>
-        public readonly long ByteOffset;
+        public static implicit operator ArrayBufferView(Int16Array t)
+        {
+            return null;
+        }
+
+        public static implicit operator ArrayBufferView(Uint16Array t)
+        {
+            return null;
+        }
+
+        public static implicit operator ArrayBufferView(Int32Array t)
+        {
+            return null;
+        }
+
+        public static implicit operator ArrayBufferView(Uint32Array t)
+        {
+            return null;
+        }
+
+        public static implicit operator ArrayBufferView(Float32Array t)
+        {
+            return null;
+        }
+
+        public static implicit operator ArrayBufferView(Float64Array t)
+        {
+            return null;
+        }
+
+        public static implicit operator ArrayBufferView(DataView t)
+        {
+            return null;
+        }
+
+
+
+        public static explicit operator Int8Array(ArrayBufferView value)
+        {
+            return default(Int8Array);
+        }
+
+        public static explicit operator Uint8Array(ArrayBufferView value)
+        {
+            return default(Uint8Array);
+        }
+
+        public static explicit operator Uint8ClampedArray(ArrayBufferView value)
+        {
+            return default(Uint8ClampedArray);
+        }
+
+        public static explicit operator Int16Array(ArrayBufferView value)
+        {
+            return default(Int16Array);
+        }
+
+        public static explicit operator Uint16Array(ArrayBufferView value)
+        {
+            return default(Uint16Array);
+        }
+
+        public static explicit operator Int32Array(ArrayBufferView value)
+        {
+            return default(Int32Array);
+        }
+
+        public static explicit operator Uint32Array(ArrayBufferView value)
+        {
+            return default(Uint32Array);
+        }
+
+        public static explicit operator Float32Array(ArrayBufferView value)
+        {
+            return default(Float32Array);
+        }
+
+        public static explicit operator Float64Array(ArrayBufferView value)
+        {
+            return default(Float64Array);
+        }
+
+        public static explicit operator DataView(ArrayBufferView value)
+        {
+            return default(DataView);
+        }
     }
 }
