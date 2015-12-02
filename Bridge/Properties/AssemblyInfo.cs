25699500500f72bc2d3c2d70686af423255f576f
Geoffrey McGill <geoff@object.net>
2015-12-02 10:52:41 -0700

    Increment assembly version number to 1.10.2. Closes #682

--- Bridge/Properties/AssemblyInfo.cs
+++ Bridge/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("76ba3097-5b23-4900-bca9-2ba3bc0afcfe")]
 [assembly: AssemblyVersion("1.10")]
-[assembly: AssemblyFileVersion("1.10.1")]
+[assembly: AssemblyFileVersion("1.10.2")]
--- Compiler/Build/Properties/AssemblyInfo.cs
+++ Compiler/Build/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("736cfee7-d77d-499a-9da0-d95efbb07fdf")]
 [assembly: AssemblyVersion("1.10")]
-[assembly: AssemblyFileVersion("1.10.1")]
+[assembly: AssemblyFileVersion("1.10.2")]
