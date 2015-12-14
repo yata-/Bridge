63ecc4176ffbaa2d2349afe1eb59a742fc617741
vladsch <vladimir@object.net>
2015-12-14 16:23:23 +0300

    [TEST] [#689] Update unit tests

--- Testing/www/js/testRunner.js
+++ Testing/www/js/testRunner.js
@@ -191,6 +191,7 @@ Bridge.define('ClientTestLibrary.RunTests', {
             QUnit.test("#671", Bridge.get(ClientTestLibrary.Bridge671).testUseCase);
             QUnit.test("#674", Bridge.get(ClientTestLibrary.Bridge674).testUseCase);
             QUnit.test("#675", Bridge.get(ClientTestLibrary.Bridge675).testUseCase);
+            QUnit.test("#693", Bridge.get(ClientTestLibrary.Bridge693).testUseCase);
             QUnit.test("#708", Bridge.get(ClientTestLibrary.Bridge708).testUseCase);
 
             QUnit.module("LINQ");
