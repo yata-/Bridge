e0a48273685119f323830ab28a808c6bfa69689b
vladsch <vladimir@object.net>
2015-12-20 15:57:11 +0300

    [TEST] [#758] Update unit tests

--- Testing/www/js/bridge.js
+++ Testing/www/js/bridge.js
@@ -5887,10 +5887,7 @@ Bridge.Class.generic('Bridge.Comparer$1', function (T) {
 
         constructor: function (fn) {
             this.fn = fn;
-        },
-
-        compare: function (x, y) {
-            return this.fn(x, y);
+            this.compare = fn;
         }
     }));
 });
@@ -6393,7 +6390,7 @@ Bridge.Class.generic('Bridge.List$1', function (T) {
 
         sort: function (comparison) {
             this.checkReadOnly();
-            this.items.sort(comparison);
+            this.items.sort(comparison || Bridge.Comparer$1.$default.compare);
         },
 
         splice: function (start, count, items) {
