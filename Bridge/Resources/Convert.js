99d4825900f74dbd8a9cde076d693161b107b355
Andrey Chechel <chechel.ao@gmail.com>
2016-01-18 22:26:34 +0300

    Covert.ToBoolean method has been implemented;

--- Bridge/Resources/Convert.js
+++ Bridge/Resources/Convert.js
@@ -1,7 +1,34 @@
 ﻿// @source Convert.js
 
 var convert = {
+    toBoolean: function (value, formatProvider) {
+        var type = typeof (value);
+        switch (type) {
+            case "boolean":
+                return value;
 
+            case "number":
+                return value !== 0; // non-zero int/float value is always converted to True;
+
+            case "string":
+                var lowCaseVal = value.toLowerCase().trim();
+                if (lowCaseVal === "true")
+                    return true;
+                else if (lowCaseVal === "false")
+                    return false;
+                else {
+                    throw new Bridge.FormatException("String was not recognized as a valid Boolean.");
+                }
+
+            case "object":
+                if (value == null)
+                    return false;
+                break;
+        }
+
+        //TODO: IConvertible 
+        throw new Bridge.NotSupportedException("IConvertible interface is not supported.");
+    }
 };
 
 Bridge.Convert = convert;
--- Bridge/System/Convert.cs
+++ Bridge/System/Convert.cs
@@ -6,5 +6,18 @@ namespace System
     [Name("Convert")]
     public static class Convert
     {
+        /// <summary>
+        /// Converts the value of a specified object to an equivalent Boolean value.
+        /// Note: Calling this method for <see cref="char"/> and <see cref="DateTime"/> values always throws <see cref="T:System.InvalidCastException"/>.
+        /// </summary>
+        [Template("Bridge.Convert.toBoolean({value}, null)")]
+        public static extern bool ToBoolean(object value);
+
+        /// <summary>
+        /// Converts the value of the specified object to an equivalent Boolean value, using the specified culture-specific formatting information.
+        /// Note: Calling this method for <see cref="char"/> and <see cref="DateTime"/> values always throws <see cref="T:System.InvalidCastException"/>.
+        /// </summary>
+        [Template("Bridge.Convert.toBoolean({value}, {provider})")]
+        public static extern bool ToBoolean(object value, IFormatProvider provider);
     }
 }
