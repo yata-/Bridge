09d4bfc277499061f39205e3d6d136197a92d36c
LeonidVeriga <leonid@object.net>
2016-01-24 16:42:55 +0300

    [TES] [#692] New test format

--- Testing/tests/client/BridgeIssues/N692.cs
+++ Testing/tests/client/BridgeIssues/N692.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Bridge;
-using Bridge.QUnit;
+using Bridge.Test;
 
-namespace ClientTestLibrary
+namespace Bridge.ClientTest.BridgeIssues
 {
     // Bridge[#796]
-    [FileName("testBridgeIssues.js")]
-    internal class Bridge692
+    [Category(Constants.MODULE_ISSUES)]
+    [TestFixture(TestNameFormat = "#692 - {0}")]
+    public class Bridge692
     {
         public struct A
         {
@@ -88,33 +89,32 @@ namespace ClientTestLibrary
             }
         }
 
-        public static void TestUseCase(Assert assert)
+        [Test(ExpectedCount = 8)]
+        public static void TestUseCase()
         {
-            assert.Expect(8);
-
             var a = new A();
-            assert.Equal(a, a, "Bridge692 A");
+            Assert.AreEqual(a, a, "Bridge692 A");
 
             var b1 = new B1();
-            assert.Equal(b1, b1, "Bridge692 B1");
+            Assert.AreEqual(b1, b1, "Bridge692 B1");
 
             var b2 = new B1();
-            assert.Equal(b2, b2, "Bridge692 B2");
+            Assert.AreEqual(b2, b2, "Bridge692 B2");
 
             var b3 = new B3();
-            assert.Equal(b3, b3, "Bridge692 B3");
+            Assert.AreEqual(b3, b3, "Bridge692 B3");
 
             var c1 = new C1();
-            assert.NotEqual(c1, c1, "Bridge692 C1");
+            Assert.AreNotStrictEqual(c1, c1, "Bridge692 C1");
 
             var c2 = new C2();
-            assert.NotEqual(c2, c2, "Bridge692 C2");
+            Assert.AreNotStrictEqual(c2, c2, "Bridge692 C2");
 
             var c3 = new C3();
-            assert.NotEqual(c3, c3, "Bridge692 C3");
+            Assert.AreNotStrictEqual(c3, c3, "Bridge692 C3");
 
             C3? c3_1 = new C3();
-            assert.NotEqual(c3_1, c3_1, "Bridge692 C3_1");
+            Assert.AreNotStrictEqual(c3_1, c3_1, "Bridge692 C3_1");
         }
     }
 }
