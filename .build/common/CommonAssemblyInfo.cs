3cb9cad8ba21bcd87c32128e4e22d97bcbe50672
Geoffrey McGill <geoff@object.net>
2016-02-12 14:25:09 -0700

    [#744] New .build\common\ folder and update location across projects

--- .build/common/CommonAssemblyInfo.cs
+++ .build/common/CommonAssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("1.11")]
 [assembly: AssemblyFileVersion("1.11.0")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
-[assembly: AssemblyCopyright("Copyright 2008-2016 Object.NET, Inc.")]
+[assembly: AssemblyCopyright("Copyright 2008-2017 Object.NET, Inc.")]
