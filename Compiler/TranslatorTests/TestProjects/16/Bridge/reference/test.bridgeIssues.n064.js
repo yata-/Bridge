095c7a5b0de7df1ab8bac5bea627369b6e1c1912
LeonidVeriga <leonid@object.net>
2016-02-14 16:42:28 +0300

    [TEST][#64] Move test from Client to Compiler [#950]

---
+++ Compiler/TranslatorTests/TestProjects/16/Bridge/reference/test.bridgeIssues.n064.js
@@ -0,0 +1,22 @@
+﻿/* global Bridge */
+
+(function (globals) {
+    "use strict";
+
+    Bridge.define('Test.BridgeIssues.N064.Class64', {
+        constructor: function () {
+        },
+        constructor$1: function (related) {
+        },
+        test: function () {
+            var aux = new Test.BridgeIssues.N064.Class64.Aux1();
+            new Test.BridgeIssues.N064.Class64("constructor$1", aux);
+        }
+    });
+    
+    Bridge.define('Test.BridgeIssues.N064.Class64.Aux1');
+    
+    
+    
+    Bridge.init();
+})(this);
--- Tests/JavaScript/Batch1/Tests/NotCompiling.cs
+++ Tests/JavaScript/Batch1/Tests/NotCompiling.cs
@@ -24,28 +24,6 @@ namespace Bridge.ClientTest.BridgeIssues
     //     }
     // }
 
-    // [#64]
-    internal class Class64
-    {
-        public class Aux1
-        {
-        }
-
-        public Class64()
-        {
-        }
-
-        public Class64(Aux1 related)
-        {
-        }
-
-        public void Test()
-        {
-            var aux = new Class64.Aux1();
-            new Class64(aux);
-        }
-    }
-
     // [#65]
     internal class Class65_1
     {
