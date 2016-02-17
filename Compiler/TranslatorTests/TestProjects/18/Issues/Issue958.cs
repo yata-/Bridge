3601621c3c896fd81ace6925763d9412dc0a37cb
LeonidVeriga <leonid@object.net>
2016-02-17 18:23:59 +0300

    [TEST] [#958] Compiler test

---
+++ Compiler/TranslatorTests/TestProjects/18/Issues/Issue958.cs
@@ -0,0 +1,58 @@
+﻿using Bridge;
+using Bridge.Html5;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//#958
+namespace TestIssue958
+{
+    public class Issue958
+    {
+        public static void Main()
+        {
+            var message = new SetValue("Hi!");
+            ProcessMessage(message);
+        }
+
+        private static void ProcessMessage(IMessage message)
+        {
+            // The call should have generic type as function parameter
+            message
+                .If<SetName>(action => Console.WriteLine("Name: " + action.Name))
+                .If<SetValue>(action => Console.WriteLine("Value: " + action.Value));
+        }
+    }
+
+    public static class MessageExtensions
+    {
+        // Should have generic type as function parameter
+        public static IMessage If<T>(this IMessage source, Action<T> work) where T : IMessage
+        {
+            if (source is T)
+                work((T)source);
+            return source;
+        }
+    }
+
+    public sealed class SetName : IMessage
+    {
+        public SetName(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; private set; }
+    }
+
+    public sealed class SetValue : IMessage
+    {
+        public SetValue(string value)
+        {
+            Value = value;
+        }
+        public string Value { get; private set; }
+    }
+
+    public interface IMessage { }
+}
