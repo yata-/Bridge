8684c2e2f281144e67b8ba1bb09f84ca808961bf
Geoffrey McGill <geoff@object.net>
2016-02-27 21:29:31 -0700

    Update @version to 1.11.1 and @date

--- .build/common/CommonAssemblyInfo.cs
+++ .build/common/CommonAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("1.11")]
-[assembly: AssemblyFileVersion("1.11.0")]
-[assembly: AssemblyInformationalVersion("1.11.0")]
+[assembly: AssemblyFileVersion("1.11.1")]
+[assembly: AssemblyInformationalVersion("1.11.1")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2008-2016 Object.NET, Inc.")]
