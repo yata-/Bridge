cbb4603c18c994fb2d042e9a187c336f4136e7bf
LeonidVeriga <leonid@object.net>
2016-02-29 15:52:15 +0300

    [TEST] [#1009] Compiler test

---
+++ Compiler/TranslatorTests/TestProjects/16/Bridge/reference/test.bridgeIssues.n1009.js
@@ -0,0 +1,21 @@
+﻿/* 
+    A custom comment
+*/
+
+(function (globals) {
+    "use strict";
+
+    Bridge.define('Test.BridgeIssues.N1009.Class1009', {
+        statics: {
+    
+        }
+    });
+    
+    
+    
+    Bridge.init();
+})(this);
+
+/* 
+    A custom comment
+*/
---
+++ Compiler/TranslatorTests/TestProjects/16/BridgeIssues/1500/N1009.cs
@@ -0,0 +1,24 @@
+﻿//#1009
+namespace Test.BridgeIssues.N1009
+{
+    using Bridge;
+
+    public class Class1009
+    {
+        [Init(InitPosition.Top)]
+        public static void Top()
+        {
+            /*
+                A custom comment
+            */
+        }
+
+        [Init(InitPosition.Bottom)]
+        public static void Bottom()
+        {
+            /*
+                A custom comment
+            */
+        }
+    }
+}
