537dbfeb1272ae566d1478b84d5be57b7e5b5a00
Dia6lo <Dgamjm@gmail.com>
2016-03-10 16:55:08 +1000

    Implement HTML5 WebSocket interface and related events.

---
+++ Html5/Events/MessageEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace Bridge.Html5
+{
+	/// <summary>
+	/// A MessageEvent interface represents a message received by a target, being a WebSocket or a WebRTC RTCDataChannel.
+	/// </summary>
+	[External]
+	[Name("MessageEvent")]
+	public class MessageEvent : Event
+	{
+		private MessageEvent()
+		{
+		}
+
+		public readonly Any<string, Blob, ArrayBuffer> Data;
+
+		public readonly string Origin;
+	}
+}
