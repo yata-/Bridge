583a172bae61b97710aecad5e91aa14d0e53e05a
Dia6lo <Dgamjm@gmail.com>
2016-03-11 16:22:56 +1000

    Add basic ClientWebSocketOptions class implementation.

--- Bridge/Resources/ClientWebSocket.js
+++ Bridge/Resources/ClientWebSocket.js
@@ -9,3 +9,27 @@
             return this.state;
         }
     });
+
+    Bridge.define("Bridge.Net.WebSockets.ClientWebSocketOptions", {
+        constructor: function() {
+            this.isReadOnly = false;
+            this.requestedSubProtocols = [];
+        },
+
+        setToReadOnly: function() {
+            if (this.isReadOnly) {
+                throw new Bridge.InvalidOperationException("Options are already readonly.");
+            }
+            this.isReadOnly = true;
+        },
+
+        addSubProtocol: function(subProtocol) {
+            if (this.isReadOnly) {
+                throw new Bridge.InvalidOperationException("Socket already started.");
+            }
+            if (this.requestedSubProtocols.indexOf(subProtocol) > -1) {
+                throw new Bridge.ArgumentException("Socket cannot have duplicate sub-protocols.", "subProtocol");
+            }
+            this.requestedSubProtocols.push(subProtocol);
+        }
+    });
