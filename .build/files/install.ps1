4475b1423dee1e2ea01bd36f0cf6d09219da82fc
Geoffrey McGill <geoff@object.net>
2016-03-12 00:07:48 -0700

    [#419] Forced addition of new PropertyGroup and NoStdLib in .csproj

--- .build/files/install.ps1
+++ .build/files/install.ps1
@@ -1,14 +1,24 @@
 ﻿#First some common params, delivered by the nuget package installer
 param($installPath, $toolsPath, $package, $project)
 
-write-output ("Setting up '" + $project.ProjectName + "'.")
+Write-Host ("Bridge.NET is configuring " + $project.ProjectName + ".")
 
 # Remove all references of object
 $project.Object.References | foreach-object {
- if ($_.Identity.ToLower().StartsWith("system") -or $_.Identity.ToLower().StartsWith("microsoft")) {
-  write-output ("Removing reference to '" + $_.Identity + "'.")
-  $_.Remove()
- }
+	if ($_.Identity.ToLower().StartsWith("system") -or $_.Identity.ToLower().StartsWith("microsoft")) {
+		Write-Host ("Removing Reference to " + $_.Identity + ".")
+		$_.Remove()
+	}
 }
 
-write-output "All done."
+# Add a new PropertyGroup with NoStdLib node.
+# See Issue #419 for more information on why.
+# https://github.com/bridgedotnet/Bridge/issues/419
+# Once Visual Studio or NuGet defect is fixed, lines 19-22 can be removed.
+
+$projectMSBuild = [Microsoft.Build.Construction.ProjectRootElement]::Open($project.FullName)
+$propertyGroup = $projectMSBuild.AddPropertyGroup()
+$propEnableNuGetImport = $propertyGroup.AddProperty('NoStdLib', 'true');
+$projectMSBuild.Save()
+
+Write-Host ("Bridge.NET installation was successful")
