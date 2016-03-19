884511c2720f893b4bee67cc8531ba259454aee2
Fabrício Zimmerer Murta <fabricio@object.net>
2016-03-18 22:13:51 -0300

    Adds uninstall.ps1 script to the source tree.
    This adds a required file to the #419 issue implementation.

---
+++ .build/files/uninstall.ps1
@@ -0,0 +1,15 @@
+﻿#First some common params, delivered by the nuget package installer
+param($installPath, $toolsPath, $package, $project)
+
+write-output ("Removing Bridge.NET specific settings on '" + $project.ProjectName + "'.")
+
+# Sets the NoStdLib setting back to default for every project configuration.
+$project.ConfigurationManager | ForEach-Object {
+ $nostdlib_setting = $_.Properties.Item("NoStdLib")
+
+ if ($nostdlib_setting.Value) {
+  $nostdlib_setting.Value = $null
+ }
+}
+
+write-output "All done."
