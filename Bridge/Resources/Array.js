ee2ebaa2e3ebfa3c074f19c59a4848e04d860a63
LeonidVeriga <leonid@object.net>
2016-03-25 12:07:31 +0300

    [FIX] [#1050] IE8 does not have `Array.indexOf` - fix according to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf

--- Bridge/Resources/Array.js
+++ Bridge/Resources/Array.js
@@ -1,5 +1,40 @@
 ﻿    // @source Array.js
 
+    // Production steps of ECMA-262, Edition 5, 15.4.4.14
+    if (!Array.prototype.indexOf) {
+        Array.prototype.indexOf = function (searchElement, fromIndex) {
+            var k;
+
+            if (this == null) {
+                throw new TypeError('"this" is null or not defined');
+            }
+
+            var o = Object(this);
+            var len = o.length >>> 0;
+            if (len === 0) {
+                return -1;
+            }
+
+            var n = +fromIndex || 0;
+            if (Math.abs(n) === Infinity) {
+                n = 0;
+            }
+
+            if (n >= len) {
+                return -1;
+            }
+
+            k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+            while (k < len) {
+                if (k in o && o[k] === searchElement) {
+                    return k;
+                }
+                k++;
+            }
+            return -1;
+        };
+    }
+
     var array = {
         toIndex: function (arr, indices) {
             if (indices.length !== (arr.$s ? arr.$s.length : 1)) {
