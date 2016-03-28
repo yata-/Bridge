b841f5112883428cd2215edf1f2896265feda6d1
LeonidVeriga <leonid@object.net>
2016-03-28 15:02:33 +0300

    Formatting install.ps1 and uninstall.ps1

--- .build/files/uninstall.ps1
+++ .build/files/uninstall.ps1
@@ -1,15 +1,18 @@
 ﻿#First some common params, delivered by the nuget package installer
 param($installPath, $toolsPath, $package, $project)
 
-write-output ("Removing Bridge.NET specific settings on '" + $project.ProjectName + "'.")
+Write-Host ("Removing Bridge.NET specific settings on '" + $project.ProjectName)
 
 # Sets the NoStdLib setting back to default for every project configuration.
+# See Issue #419 for more information on why.
+# https://github.com/bridgedotnet/Bridge/issues/419
+# Once Visual Studio or NuGet defect is fixed, lines 10-216 can be removed.
 $project.ConfigurationManager | ForEach-Object {
- $nostdlib_setting = $_.Properties.Item("NoStdLib")
+    $nostdlib_setting = $_.Properties.Item("NoStdLib")
 
- if ($nostdlib_setting.Value) {
-  $nostdlib_setting.Value = $null
- }
+    if ($nostdlib_setting.Value) {
+        $nostdlib_setting.Value = $null
+    }
 }
 
-write-output "All done."
+Write-Host ("Bridge.NET uninstallation was successful")
