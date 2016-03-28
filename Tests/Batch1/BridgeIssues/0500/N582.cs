a60ae004b2be9f750ebc6383c93d1f6d6d6b4e92
vladsch <vladimir@object.net>
2016-03-29 02:18:09 +0300

    [#869] Update unit tests

--- Tests/Batch1/BridgeIssues/0500/N582.cs
+++ Tests/Batch1/BridgeIssues/0500/N582.cs
@@ -58,21 +58,21 @@ namespace Bridge.ClientTest.BridgeIssues
         [Test(ExpectedCount = 4)]
         public static void TestSubtractTimeSpan()
         {
-            DateTime date1 = new DateTime(DateTime.Utc(1996, 6, 3, 22, 15, 0));
-            DateTime date2 = new DateTime(DateTime.Utc(1996, 12, 6, 13, 2, 0));
-            DateTime date3 = new DateTime(DateTime.Utc(1996, 10, 12, 8, 42, 0));
+            DateTime date1 = new DateTime(1996, 6, 3, 22, 15, 0);
+            DateTime date2 = new DateTime(1996, 12, 6, 13, 2, 0);
+            DateTime date3 = new DateTime(1996, 10, 12, 8, 42, 0);
 
             TimeSpan diff1 = date2.Subtract(date1);
             Assert.True(diff1.Equals(new TimeSpan(185, 14, 47, 0)), "Bridge582 TestSubtractTimeSpan diff1");
 
             DateTime date4 = date3.Subtract(diff1);
-            Assert.True(date4.Equals(new DateTime(DateTime.Utc(1996, 4, 9, 17, 55, 0))), "Bridge582 TestSubtractTimeSpan date4");
+            Assert.True(date4.Equals(new DateTime(1996, 4, 9, 17, 55, 0)), "Bridge582 TestSubtractTimeSpan date4");
 
             TimeSpan diff2 = date2 - date3;
             Assert.True(diff2.Equals(new TimeSpan(55, 4, 20, 0)), "Bridge582 TestSubtractTimeSpan diff2");
 
             DateTime date5 = date1 - diff2;
-            Assert.True(date5.Equals(new DateTime(DateTime.Utc(1996, 4, 9, 17, 55, 0))), "Bridge582 TestSubtractTimeSpan date5");
+            Assert.True(date5.Equals(new DateTime(1996, 4, 9, 17, 55, 0)), "Bridge582 TestSubtractTimeSpan date5");
         }
 
         [Test(ExpectedCount = 6)]
