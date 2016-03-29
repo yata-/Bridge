fb3c0002474cb33d8a88a76879d5c0d508b44ef4
Andrey Chechel <chechel.ao@gmail.com>
2016-03-29 04:00:29 +0300

    Regex Escape/Unescape methods, unit tests

--- Bridge/Resources/Text/RegularExpressions/RegexParser.js
+++ Bridge/Resources/Text/RegularExpressions/RegexParser.js
@@ -2,12 +2,109 @@
 
 Bridge.define("Bridge.Text.RegularExpressions.RegexParser", {
     statics: {
+
+        _Q : 5,     // quantifier
+        _S : 4,     // ordinary stoppper
+        _Z : 3,     // ScanBlank stopper
+        _X : 2,     // whitespace
+        _E : 1,     // should be escaped
+
+        _category: [
+            //  0 1 2 3 4 5 6 7 8 9 A B C D E F 0 1 2 3 4 5 6 7 8 9 A B C D E F 
+                0,0,0,0,0,0,0,0,0,2,2,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            //  ! " # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? 
+                2,0,0,3,4,0,0,0,4,4,5,5,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,
+            //  @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,4,0,4,0,
+            //  ' a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~ 
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,4,0,0,0],
+
         escape: function(input) {
-            return ""; //TODO: Implement
+            var statics = Bridge.get(Bridge.Text.RegularExpressions.RegexParser);
+
+            for (var i = 0; i < input.length; i++) {
+                if (statics._isMetachar(input[i])) {
+                    var sb = "";
+                    var ch = input[i];
+                    var lastpos;
+
+                    sb += input.slice(0, i);
+
+                    do {
+                        sb += "\\";
+
+                        switch (ch) {
+                            case "\n":
+                                ch = "n";
+                                break;
+                            case "\r":
+                                ch = "r";
+                                break;
+                            case "\t":
+                                ch = "t";
+                                break;
+                            case "\f":
+                                ch = "f";
+                                break;
+                        }
+                        sb += ch;
+                        i++;
+                        lastpos = i;
+ 
+                        while (i < input.length) {
+                            ch = input[i];
+                            if (statics._isMetachar(ch)) {
+                                break;
+                            }
+ 
+                            i++;
+                        }
+ 
+                        sb += input.slice(lastpos, i);
+ 
+                    } while (i < input.length);
+ 
+                    return sb;
+                }
+            }
+ 
+            return input;
         },
 
         unescape: function(input) {
-            return ""; //TODO: Implement
+            for (var i = 0; i < input.length; i++) {
+                if (input[i] === "\\") {
+                    var sb = "";
+                    var culture = Bridge.get(Bridge.CultureInfo).invariantCulture;
+                    var p = new Bridge.Text.RegularExpressions.RegexParser(culture);
+
+                    var lastpos;
+                    p._setPattern(input);
+ 
+                    sb += input.slice(0, i);
+                    do {
+                        i++;
+
+                        p._textto(i);
+                        if (i < input.length) {
+                            sb += p._scanCharEscape();
+                        }
+
+                        i = p._textpos();
+                        lastpos = i;
+                        while (i < input.length && input[i] !== "\\") {
+                            i++;
+                        }
+
+                        sb += input.slice(lastpos, i);
+ 
+                    } while (i < input.length);
+ 
+                    return sb;
+                }
+            }
+ 
+            return input;
         },
 
         parseReplacement: function(rep, caps, capsize, capnames, op) {
@@ -20,6 +117,13 @@ Bridge.define("Bridge.Text.RegularExpressions.RegexParser", {
 
             var root = p._scanReplacement();
             return new Bridge.Text.RegularExpressions.RegexReplacement(rep, root, caps);
+        },
+
+        _isMetachar: function (ch) {
+            var statics = Bridge.get(Bridge.Text.RegularExpressions.RegexParser);
+
+            var code = ch.charCodeAt(0);
+            return (code <= "|".charCodeAt(0) && statics._category[code] >= statics._E);
         }
     },
 
@@ -262,6 +366,99 @@ Bridge.define("Bridge.Text.RegularExpressions.RegexParser", {
         return i;
     },
 
+    _scanOctal: function() {
+        var d;
+        var i;
+        var c;
+ 
+        // Consume octal chars only up to 3 digits and value 0377
+ 
+        c = 3;
+ 
+        if (c > this._charsRight()) {
+            c = this._charsRight();
+        }
+ 
+        for (i = 0; c > 0 && (d = this._rightChar() - "0") <= 7; c -= 1) {
+            this._moveRight();
+            
+            i *= 8;
+            i += d;
+            if (this._useOptionE() && i >= 0x20) {
+                break;
+            }
+        }
+ 
+        // Octal codes only go up to 255.  Any larger and the behavior that Perl follows
+        // is simply to truncate the high bits. 
+        i &= 0xFF;
+            
+        return i;
+    },
+
+    _scanHex: function(c) {
+        var i;
+        var d;
+ 
+        i = 0;
+ 
+        if (this._charsRight() >= c) {
+            for (; c > 0 && ((d = this._hexDigit(this._moveRightGetChar())) >= 0) ; c -= 1) {
+                i *= 0x10;
+                i += d;
+            }
+        }
+ 
+        if (c > 0) {
+            throw this._makeException("Insufficient hexadecimal digits.");
+        }
+ 
+        return i;
+    },
+
+    _hexDigit: function(ch) {
+        var d;
+
+        var code = ch.charCodeAt(0);
+ 
+        if ((d = code - "0".charCodeAt(0)) <= 9) {
+            return d;
+        }
+ 
+        if ((d = code - "a".charCodeAt(0)) <= 5) {
+            return d + 0xa;
+        }
+ 
+        if ((d = code - "A".charCodeAt(0)) <= 5) {
+            return d + 0xa;
+        }
+ 
+        return -1;
+    },
+
+    _scanControl: function () {
+        var ch;
+ 
+        if (this._charsRight() <= 0) {
+            throw this._makeException("Missing control character.");
+        }
+ 
+        ch = this._moveRightGetChar();
+ 
+        // \ca interpreted as \cA
+
+        var code = ch.charCodeAt(0);
+        if (code >= "a".charCodeAt(0) && code <= "z".charCodeAt(0)) {
+            code = code - ("a".charCodeAt(0) - "A".charCodeAt(0));
+        }
+ 
+        if ((code = (code - "@".charCodeAt(0))) < " ".charCodeAt(0)) {
+            return String.fromCharCode(code);
+        }
+ 
+        throw this._makeException("Unrecognized control character.");
+    },
+
     _scanCapname: function() {
         var startpos = this._textpos();
  
@@ -273,6 +470,45 @@ Bridge.define("Bridge.Text.RegularExpressions.RegexParser", {
         }
  
         return _pattern.slice(startpos, this._textpos());
+    },
+
+    _scanCharEscape: function() {
+        var ch = this._moveRightGetChar();
+ 
+        if (ch >= "0" && ch <= "7") {
+            this._moveLeft();
+            return this._scanOctal();
+        }
+ 
+        switch (ch) {
+            case "x":
+                return this._scanHex(2);
+            case "u":
+                return this._scanHex(4);
+            case "a":
+                return "\u0007";
+            case "b":
+                return "\b";
+            case "e":
+                return "\u001B";
+            case "f":
+                return "\f";
+            case "n":
+                return "\n";
+            case "r":
+                return "\r";
+            case "t":
+                return "\t";
+            case "v":
+                return "\u000B";
+            case "c":
+                return this._scanControl();
+            default:
+                if (!this._useOptionE() && this._isWordChar(ch)) {
+                    throw this._makeException("Unrecognized escape sequence.");
+                }
+                return ch;
+        }
     },
 
     _captureSlotFromName: function (capname) {
