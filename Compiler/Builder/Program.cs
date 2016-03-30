ecfdbaf0a827606fd11f86df5a0705a8c850d992
Fabrício Zimmerer Murta <fabricio@object.net>
2016-03-30 16:16:33 -0300

    Reviews Bridge.Builder.exe commandline arguments.
    This reviews all Bridge.Builder.exe console commandline arguments
    according to docopr (http://docopt.org) guidelines.
    
    It retains though backwards compatibility with the current commandline
    options, but will no longer document nor support them, so previous
    versions do not break an newer versions use the new arguments format.
    
    It now discerns between .dll (lib) or .csproj (project) arguments and
    ensures only one of them are specified in the commandline arguments.
    
    It also supports the -- argument to stop reading for further commandline
    arguments (useful for example, on the rare case where a file name may
    start with "--").
    
    Additionally, the program now returns '1' when an error occurred and 0
    when either help was specified (and shown) or the translation finishes
    successfully.
    
    This addresses issues:
    - #1054 - return status different than 0 on error
    - #1126 - list all actual available commandline switches (except
      deprecated ones)
    - #1127 - add a -h commandline option to explicitly show help (also adds
      --help according to docopt guidelines!)

--- Compiler/Builder/Program.cs
+++ Compiler/Builder/Program.cs
@@ -10,9 +10,9 @@ namespace Bridge.Builder
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            var logger = new Logger(null, true, LoggerLevel.Trace, new ConsoleLoggerWriter(), new SimpleFileLoggerWriter());
+            var logger = new Logger(null, false, LoggerLevel.Trace, new ConsoleLoggerWriter());
 
             string projectLocation = null;
             string outputLocation = null;
@@ -28,21 +28,8 @@ namespace Bridge.Builder
 
             if (args.Length == 0)
             {
-                logger.Info("Bridge.Builder commands:");
-                logger.Info("-p or -project           Path to csproj file (required)");
-                logger.Info("-o or -output            Output directory for generated script");
-                logger.Info("-cfg or -configuration   Configuration name, typically Debug/Release");
-                logger.Info("-r or -rebuild           Force assembly rebuilding");
-                logger.Info("-nocore                  Do not extract core javascript files");
-                logger.Info("-def or -define          Defines project constants. For example, \"CONSTANT1;CONSTANT2\" ");
-#if DEBUG
-                // This code and logic is only compiled in when building bridge.net in Debug configuration
-                logger.Info("-d or -debug             Attach the builder to an visual studio debugging instance.");
-                logger.Info("                         Use this to attach the process to an open Bridge.NET solution.");
-                logger.Info("                         This option is equivalent to Build.dll's 'AttachDebugger'.");
-#endif
-                logger.Info("");
-                return;
+                showHelp();
+                return 1; // error: arguments not provided, so can't guess what to do
             }
 
             int i = 0;
@@ -51,68 +38,131 @@ namespace Bridge.Builder
             {
                 switch (args[i])
                 {
+                    // backwards compatibility -- now is non-switch argument to builder
                     case "-p":
                     case "-project":
+                        if (lib != null)
+                        {
+                            Console.WriteLine("Error: Project and assembly file specification is mutually exclusive.");
+                            showHelp();
+                            return 1;
+                        };
                         projectLocation = args[++i];
                         break;
 
                     case "-b":
-                    case "-bridge":
+                    case "-bridge": // backwards compatibility
+                    case "--bridge":
                         bridgeLocation = args[++i];
                         break;
 
                     case "-o":
-                    case "-output":
+                    case "-output": // backwards compatibility
+                    case "--output":
                         outputLocation = args[++i];
                         break;
 
-                    case "-cfg":
-                    case "-configuration":
+                    case "-c":
+                    case "-cfg": // backwards compatibility
+                    case "-configuration": // backwards compatibility
+                    case "--configuration":
                         cfg = args[++i];
                         break;
 
-                    case "-def":
-                    case "-define":
+                    case "-def": // backwards compatibility
+                    case "-D":
+                    case "-define": // backwards compatibility
+                    case "--define":
                         def = args[++i];
                         break;
 
-                    case "-rebuild":
+                    case "-rebuild": // backwards compatibility
+                    case "--rebuild":
                     case "-r":
                         rebuild = true;
                         break;
 
-                    case "-nocore":
+                    case "-nocore": // backwards compatibility
+                    case "--nocore":
                         extractCore = false;
                         break;
 
-                    case "-src":
+                    case "-s":
+                    case "-src": // backwards compatibility
+                    case "--source":
                         source = args[++i];
                         break;
 
-                    case "-folder":
+                    case "-f":
+                    case "-folder": // backwards compatibility
+                    case "--folder":
                         folder = Path.Combine(Environment.CurrentDirectory, args[++i]);
                         break;
 
-                    case "-recursive":
+                    case "-R":
+                    case "-recursive": // backwards compatibility
+                    case "--recursive":
                         recursive = true;
                         break;
 
-                    case "-lib":
+                    case "-lib": // backwards compatibility -- now is non-switch argument to builder
+                        if (projectLocation != null)
+                        {
+                            Console.WriteLine("Error: Project and assembly file specification is mutually exclusive.");
+                            showHelp();
+                            return 1;
+                        }
                         lib = args[++i];
                         break;
+
+                    case "-h":
+                    case "--help":
+                        showHelp();
+                        return 0; // success. Asked for help. Help provided.
+
 #if DEBUG
                     case "-debug":
+                    case "--debug":
                     case "-attachdebugger":
+                    case "--attachdebugger":
                     case "-d":
                         System.Diagnostics.Debugger.Launch();
                         break;
 #endif
+                    case "--": // stop reading commandline arguments
+                        // Only non-hyphen commandline argument accepted is the file name of the project or
+                        // assembly file, so if not provided already, when this option is specified, check if
+                        // it is still needed and bind the file to the correct location
+                        if (i < (args.Length-1))
+                        {
+                            // don't care about success. If not set already, then try next cmdline argument
+                            // as the file parameter and ignore following arguments, if any.
+                            bindArgToVar(args[i + 1], ref projectLocation, ref lib);
+                        }
+                        i = args.Length; // move to the end of arguments list
+                        break;
+
                     default:
-                        logger.Info("Unknown command: " + args[i]);
-                        return;
+
+                    // If this argument does not look like a cmdline switch and
+                        // neither backwards -project nor -lib were specified
+                        if (!bindArgToVar(args[i], ref projectLocation, ref lib))
+                        {
+                            Console.WriteLine("Invalid argument: " + args[i]);
+                            showHelp();
+                            return 1;
+                        }
+                        break;
                 }
 
                 i++;
+            }
+
+            if (projectLocation == null && lib == null)
+            {
+                Console.WriteLine("Error: Project or assembly file name must be specified.");
+                showHelp();
+                return 1;
             }
 
             if (string.IsNullOrEmpty(outputLocation))
@@ -121,15 +171,17 @@ namespace Bridge.Builder
             }
 
             Bridge.Translator.Translator translator = null;
+
             try
             {
                 logger.Name = "Bridge.Builder.Console";
 
-                logger.Info("Generating script...");
+                Console.WriteLine("Generating script...");
 
-                logger.Info("Command line arguments:");
-                logger.Info("\t" + (string.Join(" ", args) ?? ""));
+                Console.WriteLine("Command line arguments:");
+                Console.WriteLine("\t" + (string.Join(" ", args) ?? ""));
 
+                // FIXME: detect by extension whether first argument is a project or DLL
                 if (!string.IsNullOrWhiteSpace(projectLocation))
                 {
                     translator = new Bridge.Translator.Translator(projectLocation);
@@ -182,20 +234,21 @@ namespace Bridge.Builder
 
                 if (extractCore)
                 {
-                    logger.Info("Extracting core scripts...");
+                    Console.WriteLine("Extracting core scripts...");
                     translator.ExtractCore(outputPath);
                 }
 
-                logger.Info("Saving to " + outputPath);
+                Console.WriteLine("Saving to " + outputPath);
                 translator.SaveTo(outputPath, Path.GetFileName(outputLocation));
                 translator.Flush(outputPath, Path.GetFileName(outputLocation));
                 translator.Plugins.AfterOutput(translator, outputPath, !extractCore);
 
-                logger.Info("Done translation console");
+                Console.WriteLine("Done translating Bridge files.");
             }
             catch (EmitterException ex)
             {
                 logger.Error(string.Format("Error: {2} ({3}, {4}) {0} {1}", ex.Message, ex.StackTrace, ex.FileName, ex.StartLine, ex.StartColumn, ex.EndLine, ex.EndColumn));
+                return 1;
             }
             catch (Exception ex)
             {
@@ -216,9 +269,63 @@ namespace Bridge.Builder
                         ine = ine.InnerException;
                     }
                 }
-
-                Console.ReadLine();
+                return 1;
             }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Commandline arguments based on http://docopt.org/
+        /// </summary>
+        private static void showHelp()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            string programName = Path.GetFileName(codeBase);
+
+            Console.WriteLine(@"Usage: " + programName + @" [options] (<project-file>|<assembly-file>)
+       " + programName + @" [-h|--help]
+
+-h --help                  This help message.
+-c --configuration <name>  Configuration name (Debug/Release) [default: Debug].
+-r --rebuild               Force assembly rebuilding.
+--nocore                   Do not extract core javascript files.
+-D --define <const-list>   Semicolon-delimited list of project constants.
+-b --bridge <file>         Bridge.dll file location (currently unused).
+-s --source <file>         Source files name/pattern [default: *.cs].
+-f --folder <path>         Builder working directory relative to current WD
+                           [default: current wd].
+-R --recursive             Recursively search for .cs source files inside
+                           current workind directory.");
+
+#if DEBUG
+            // This code and logic is only compiled in when building bridge.net in Debug configuration
+            Console.WriteLine(@"-d --debug                 Attach the builder to a visual studio debugging
+                           session. Use this to attach the process to an
+                           open Bridge.NET solution. This option is equivalent
+                           to Build.dll's 'AttachDebugger'.");
+#endif
+        }
+
+        private static bool bindArgToVar(string arg, ref string proj, ref string lib)
+        {
+            if (proj == null && lib == null)
+            {
+                Console.WriteLine("hey");
+                if (arg.ToLower().EndsWith(".csproj"))
+                {
+                    Console.WriteLine("prj");
+                    proj = arg;
+                    return true;
+                }
+                else if (arg.ToLower().EndsWith(".dll"))
+                {
+                    Console.WriteLine("lib");
+                    lib = arg;
+                    return true;
+                }
+            }
+            return false; // didn't bind anywhere
         }
     }
 }
