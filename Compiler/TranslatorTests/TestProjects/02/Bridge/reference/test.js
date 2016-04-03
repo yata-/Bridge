cc2e01224c45d0a70f0a749612803e5147dece4b
LeonidVeriga <leonid@object.net>
2016-04-03 23:01:54 +0300

    Update client client test js output and compiler test js reference

--- Compiler/TranslatorTests/TestProjects/02/Bridge/reference/test.js
+++ Compiler/TranslatorTests/TestProjects/02/Bridge/reference/test.js
@@ -16697,7 +16697,7 @@ Bridge.define("Bridge.Text.RegularExpressions.RegexReplacement", {
 
 Bridge.define("Bridge.Text.RegularExpressions.RegexNetEngine", {
     statics: {
-        jsRegex: function (text, textStart, pattern, isMultiLine, isCaseInsensitive, returnAllMatches) {
+        jsRegex: function (text, textStart, pattern, isMultiLine, isCaseInsensitive, returnAllMatches, re) {
             if (text == null) {
                 throw new Bridge.ArgumentNullException("text");
             }
@@ -16732,6 +16732,9 @@ Bridge.define("Bridge.Text.RegularExpressions.RegexNetEngine", {
                 do {
                     matches.push(match);
                     match = jsRegExp.exec(text);
+                    if (match != null && re) {
+                        re._checkTimeout();
+                    }
                 } while (match != null)
 
                 return matches;
@@ -17107,7 +17110,7 @@ Bridge.define("Bridge.Text.RegularExpressions.RegexNetEngine", {
         this._checkTimeout();
 
         // The 1st run (to know the total capture):
-        var total = Bridge.Text.RegularExpressions.RegexNetEngine.jsRegex(this._text, this._textStart, this._pattern, this._isMultiLine, this._isCaseInsensitive, false);
+        var total = Bridge.Text.RegularExpressions.RegexNetEngine.jsRegex(this._text, this._textStart, this._pattern, this._isMultiLine, this._isCaseInsensitive, false, this);
         if (total == null) {
             return match;
         }
@@ -17273,7 +17276,7 @@ Bridge.define("Bridge.Text.RegularExpressions.RegexNetEngine", {
             // For repeating groups - find all captures:
             var qCh = groupDesc.quantifier[0];
             if (qCh === "*" || qCh === "+" || qCh === "{") {
-                var capMatches = Bridge.Text.RegularExpressions.RegexNetEngine.jsRegex(group.valueFull, 0, groupDesc.expr, this._isMultiLine, this._isCaseInsensitive, true);
+                var capMatches = Bridge.Text.RegularExpressions.RegexNetEngine.jsRegex(group.valueFull, 0, groupDesc.expr, this._isMultiLine, this._isCaseInsensitive, true, this);
                 if (capMatches == null) {
                     throw new Bridge.InvalidOperationException("Can't identify captures for the already matched group.");
                 }
@@ -17330,7 +17333,7 @@ Bridge.define("Bridge.Text.RegularExpressions.RegexNetEngine", {
         var restExpr = pattern.slice(subExprEndIndex, patternEndIndex);
         var transformedPattern = subExpr + "(?=" + restExpr + ")";
 
-        var subExpRes = Bridge.Text.RegularExpressions.RegexNetEngine.jsRegex(text, textOffset, transformedPattern, this._isMultiLine, this._isCaseInsensitive, false);
+        var subExpRes = Bridge.Text.RegularExpressions.RegexNetEngine.jsRegex(text, textOffset, transformedPattern, this._isMultiLine, this._isCaseInsensitive, false, this);
         if (subExpRes != null) {
             return {
                 capture: subExpRes[0],
