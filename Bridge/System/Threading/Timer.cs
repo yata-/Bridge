e181bd1c3527124dffe738397b50a63c2cef089a
LeonidVeriga <leonid@object.net>
2016-04-06 14:07:42 +0300

    [#1013] Correct `Timer` xml doc

--- Bridge/System/Threading/Timer.cs
+++ Bridge/System/Threading/Timer.cs
@@ -25,157 +25,86 @@ namespace System.Threading
     /// <summary>
     /// Provides a mechanism for executing a method at specified intervals. This class cannot be inherited.
     /// </summary>
+    /// <remarks>
+    /// <example>
+    /// <code>
+    /// TimerCallback callback = (o) => { Html5.Window.Alert(o.ToString()); };
+    /// var timer = new Timer(callback, "SomeState", 500, 500);
+    /// await Task.Delay(200); // It allows the timer to work
+    /// timer.Change(-1, 200); // Stops the timer
+    /// timer.Dispose() // Stops the timer "forever"
+    /// </code>
+    /// Timer implemented based on setTimeout() see https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setTimeout#Notes.
+    /// It's important to note that the function or code snippet cannot be executed until the thread that called setTimeout() has terminated.
+    /// The delegate specified by the callback parameter is invoked once after dueTime elapses,
+    /// and thereafter each time the period time interval elapses.
+    /// If dueTime is zero (0), callback is invoked immediately. If dueTime is Timeout.Infinite, callback is not invoked;
+    /// the timer is disabled, but can be re-enabled by calling the Change method.
+    /// Successive setTimeout() calls with delay smaller than the "minimum delay" limit are forced to use at least the minimum delay.
+    /// The minimum delay, DOM_MIN_TIMEOUT_VALUE, is 4 ms (stored in a preference in Firefox: dom.min_timeout_value),
+    /// with a DOM_CLAMP_TIMEOUT_NESTING_LEVEL of 5.
+    /// If period is zero (0) or Timeout.Infinite and dueTime is not Timeout.Infinite, callback is invoked once;
+    /// the periodic behavior of the timer is disabled, but can be re-enabled using the Change method.
+    /// </example>
+    /// </remarks>
     [External]
     [Namespace("Bridge.Threading")]
     public sealed class Timer : IDisposable
     {
         /// <summary>
         /// Initializes a new instance of the Timer class, using a 32-bit signed integer to specify the time interval.
+        /// The delegate specified by the callback parameter is invoked once after dueTime elapses, and thereafter each time the period time interval elapses.
+        /// If dueTime is zero (0), callback is invoked immediately.If dueTime is Timeout.Infinite, callback is not invoked; the timer is disabled, but can be re-enabled by calling the Change method.
         /// </summary>
         /// <param name="callback">A TimerCallback delegate representing a method to be executed.</param>
         /// <param name="state">An object containing information to be used by the callback method, or null.</param>
         /// <param name="dueTime">The amount of time to delay before callback is invoked, in milliseconds. Specify Timeout.Infinite to prevent the timer from starting. Specify zero (0) to start the timer immediately.</param>
         /// <param name="period">The time interval between invocations of callback, in milliseconds. Specify Timeout.Infinite to disable periodic signaling.</param>
-        /// <remarks>
-        /// Timer implemented based on setTimeout() see https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setTimeout#Notes.
-        /// It's important to note that the function or code snippet cannot be executed until the thread that called setTimeout() has terminated.
-        /// <code>
-        /// TimerCallback callback = (o) => { Html5.Window.Alert(o.ToString()); };
-        /// var timer = new Timer(callback, "SomeState", 500, 500);
-        /// await Task.Delay(200); // It allows the timer to work
-        /// timer.Change(-1, 200); // Stops the timer
-        /// timer.Dispose() // Stops the timer "forever"
-        /// </code>
-        /// The delegate specified by the callback parameter is invoked once after dueTime elapses,
-        /// and thereafter each time the period time interval elapses.
-        /// If dueTime is zero (0), callback is invoked immediately. If dueTime is Timeout.Infinite, callback is not invoked;
-        /// the timer is disabled, but can be re-enabled by calling the Change method.
-        /// Successive setTimeout() calls with delay smaller than the "minimum delay" limit are forced to use at least the minimum delay.
-        /// The minimum delay, DOM_MIN_TIMEOUT_VALUE, is 4 ms (stored in a preference in Firefox: dom.min_timeout_value),
-        /// with a DOM_CLAMP_TIMEOUT_NESTING_LEVEL of 5.
-        /// If period is zero (0) or Timeout.Infinite and dueTime is not Timeout.Infinite, callback is invoked once;
-        /// the periodic behavior of the timer is disabled, but can be re-enabled using the Change method.
-        /// </remarks>
         [Template("new Bridge.Threading.Timer(\"constructor$1\", {callback}, {state}, {dueTime}, {period})")]
         public extern Timer(TimerCallback callback, Object state, int dueTime, int period);
 
         /// <summary>
-        /// Initializes a new instance of the Timer class, using a 32-bit signed integer to specify the time interval.
+        /// Initializes a new instance of the Timer class, using TimeSpan values to measure time intervals.
+        /// The delegate specified by the callback parameter is invoked once after dueTime elapses, and thereafter each time the period time interval elapses.
+        /// If dueTime is zero (0), callback is invoked immediately.If dueTime is negative one (-1) milliseconds, callback is not invoked; the timer is disabled, but can be re-enabled by calling the Change method.
         /// </summary>
         /// <param name="callback">A TimerCallback delegate representing a method to be executed.</param>
         /// <param name="state">An object containing information to be used by the callback method, or null.</param>
         /// <param name="dueTime">The amount of time to delay before callback is invoked, in milliseconds. Specify Timeout.Infinite to prevent the timer from starting. Specify zero (0) to start the timer immediately.</param>
         /// <param name="period">The time interval between invocations of callback, in milliseconds. Specify Timeout.Infinite to disable periodic signaling.</param>
-        /// <remarks>
-        /// Timer implemented based on setTimeout() see https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setTimeout#Notes.
-        /// It's important to note that the function or code snippet cannot be executed until the thread that called setTimeout() has terminated.
-        /// <code>
-        /// TimerCallback callback = (o) => { Html5.Window.Alert(o.ToString()); };
-        /// var timer = new Timer(callback, "SomeState", 500, 500);
-        /// await Task.Delay(200); // It allows the timer to work
-        /// timer.Change(-1, 200); // Stops the timer
-        /// timer.Dispose() // Stops the timer "forever"
-        /// </code>
-        /// The delegate specified by the callback parameter is invoked once after dueTime elapses,
-        /// and thereafter each time the period time interval elapses.
-        /// If dueTime is zero (0), callback is invoked immediately. If dueTime is Timeout.Infinite, callback is not invoked;
-        /// the timer is disabled, but can be re-enabled by calling the Change method.
-        /// Successive setTimeout() calls with delay smaller than the "minimum delay" limit are forced to use at least the minimum delay.
-        /// The minimum delay, DOM_MIN_TIMEOUT_VALUE, is 4 ms (stored in a preference in Firefox: dom.min_timeout_value),
-        /// with a DOM_CLAMP_TIMEOUT_NESTING_LEVEL of 5.
-        /// If period is zero (0) or Timeout.Infinite and dueTime is not Timeout.Infinite, callback is invoked once;
-        /// the periodic behavior of the timer is disabled, but can be re-enabled using the Change method.
-        /// </remarks>
         [Template("new Bridge.Threading.Timer(\"constructor$3\", {callback}, {state}, {dueTime}, {period})")]
         public extern Timer(TimerCallback callback, Object state, TimeSpan dueTime, TimeSpan period);
 
-
         /// <summary>
         /// Initializes a new instance of the Timer class, using 32-bit unsigned integers to measure time intervals.
+        /// The delegate specified by the callback parameter is invoked once after dueTime elapses, and thereafter each time the period time interval elapses.
+        /// If dueTime is zero (0), callback is invoked immediately.If dueTime is Timeout.Infinite, callback is not invoked; the timer is disabled, but can be re-enabled by calling the Change method.
         /// </summary>
         /// <param name="callback">A TimerCallback delegate representing a method to be executed.</param>
         /// <param name="state">An object containing information to be used by the callback method, or null.</param>
         /// <param name="dueTime">The amount of time to delay before callback is invoked, in milliseconds. Specify Timeout.Infinite to prevent the timer from starting. Specify zero (0) to start the timer immediately.</param>
         /// <param name="period">The time interval between invocations of callback, in milliseconds. Specify Timeout.Infinite to disable periodic signaling.</param>
-        /// <remarks>
-        /// Timer implemented based on setTimeout() see https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setTimeout#Notes.
-        /// It's important to note that the function or code snippet cannot be executed until the thread that called setTimeout() has terminated.
-        /// <code>
-        /// TimerCallback callback = (o) => { Html5.Window.Alert(o.ToString()); };
-        /// var timer = new Timer(callback, "SomeState", 500, 500);
-        /// await Task.Delay(200); // It allows the timer to work
-        /// timer.Change(-1, 200); // Stops the timer
-        /// timer.Dispose() // Stops the timer "forever"
-        /// </code>
-        /// The delegate specified by the callback parameter is invoked once after dueTime elapses,
-        /// and thereafter each time the period time interval elapses.
-        /// If dueTime is zero (0), callback is invoked immediately. If dueTime is Timeout.Infinite, callback is not invoked;
-        /// the timer is disabled, but can be re-enabled by calling the Change method.
-        /// Successive setTimeout() calls with delay smaller than the "minimum delay" limit are forced to use at least the minimum delay.
-        /// The minimum delay, DOM_MIN_TIMEOUT_VALUE, is 4 ms (stored in a preference in Firefox: dom.min_timeout_value),
-        /// with a DOM_CLAMP_TIMEOUT_NESTING_LEVEL of 5.
-        /// If period is zero (0) or Timeout.Infinite and dueTime is not Timeout.Infinite, callback is invoked once;
-        /// the periodic behavior of the timer is disabled, but can be re-enabled using the Change method.
-        /// </remarks>
         [Template("new Bridge.Threading.Timer(\"constructor$4\", {callback}, {state}, {dueTime}, {period})")]
         public extern Timer(TimerCallback callback, Object state, UInt32 dueTime, UInt32 period);
 
         /// <summary>
         /// Initializes a new instance of the Timer class, using 64-bit signed integers to measure time intervals.
+        /// The delegate specified by the callback parameter is invoked once after dueTime elapses, and thereafter each time the period time interval elapses.
+        /// If dueTime is zero (0), callback is invoked immediately.If dueTime is Timeout.Infinite, callback is not invoked; the timer is disabled, but can be re-enabled by calling the Change method.
         /// </summary>
         /// <param name="callback">A TimerCallback delegate representing a method to be executed.</param>
         /// <param name="state">An object containing information to be used by the callback method, or null.</param>
         /// <param name="dueTime">The amount of time to delay before callback is invoked, in milliseconds. Specify Timeout.Infinite to prevent the timer from starting. Specify zero (0) to start the timer immediately.</param>
         /// <param name="period">The time interval between invocations of callback, in milliseconds. Specify Timeout.Infinite to disable periodic signaling.</param>
-        /// <remarks>
-        /// Timer implemented based on setTimeout() see https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setTimeout#Notes.
-        /// It's important to note that the function or code snippet cannot be executed until the thread that called setTimeout() has terminated.
-        /// <code>
-        /// TimerCallback callback = (o) => { Html5.Window.Alert(o.ToString()); };
-        /// var timer = new Timer(callback, "SomeState", 500, 500);
-        /// await Task.Delay(200); // It allows the timer to work
-        /// timer.Change(-1, 200); // Stops the timer
-        /// timer.Dispose() // Stops the timer "forever"
-        /// </code>
-        /// The delegate specified by the callback parameter is invoked once after dueTime elapses,
-        /// and thereafter each time the period time interval elapses.
-        /// If dueTime is zero (0), callback is invoked immediately. If dueTime is Timeout.Infinite, callback is not invoked;
-        /// the timer is disabled, but can be re-enabled by calling the Change method.
-        /// Successive setTimeout() calls with delay smaller than the "minimum delay" limit are forced to use at least the minimum delay.
-        /// The minimum delay, DOM_MIN_TIMEOUT_VALUE, is 4 ms (stored in a preference in Firefox: dom.min_timeout_value),
-        /// with a DOM_CLAMP_TIMEOUT_NESTING_LEVEL of 5.
-        /// If period is zero (0) or Timeout.Infinite and dueTime is not Timeout.Infinite, callback is invoked once;
-        /// the periodic behavior of the timer is disabled, but can be re-enabled using the Change method.
-        /// </remarks>
         [Template("new Bridge.Threading.Timer(\"constructor$2\", {callback}, {state}, {dueTime}, {period})")]
         public extern Timer(TimerCallback callback, Object state, long dueTime, long period);
 
         /// <summary>
         /// Initializes a new instance of the Timer class with an infinite period and an infinite due time, using the newly created Timer object as the state object.
-        /// </summary>
-        /// <param name="callback">A TimerCallback delegate representing a method to be executed.</param>
-        /// <remarks>
         /// Call this constructor when you want to use the Timer object itself as the state object. After creating the timer, use the Change method to set the interval and due time.
         /// This constructor specifies an infinite due time before the first callback and an infinite interval between callbacks, in order to prevent the first callback from occurring before the Timer object is assigned to the state object.
-        /// Timer implemented based on setTimeout() see https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setTimeout#Notes.
-        /// It's important to note that the function or code snippet cannot be executed until the thread that called setTimeout() has terminated.
-        /// <code>
-        /// TimerCallback callback = (o) => { Html5.Window.Alert(o.ToString()); };
-        /// var timer = new Timer(callback, "SomeState", 500, 500);
-        /// await Task.Delay(200); // It allows the timer to work
-        /// timer.Change(-1, 200); // Stops the timer
-        /// timer.Dispose() // Stops the timer "forever"
-        /// </code>
-        /// The delegate specified by the callback parameter is invoked once after dueTime elapses,
-        /// and thereafter each time the period time interval elapses.
-        /// If dueTime is zero (0), callback is invoked immediately. If dueTime is Timeout.Infinite, callback is not invoked;
-        /// the timer is disabled, but can be re-enabled by calling the Change method.
-        /// Successive setTimeout() calls with delay smaller than the "minimum delay" limit are forced to use at least the minimum delay.
-        /// The minimum delay, DOM_MIN_TIMEOUT_VALUE, is 4 ms (stored in a preference in Firefox: dom.min_timeout_value),
-        /// with a DOM_CLAMP_TIMEOUT_NESTING_LEVEL of 5.
-        /// If period is zero (0) or Timeout.Infinite and dueTime is not Timeout.Infinite, callback is invoked once;
-        /// the periodic behavior of the timer is disabled, but can be re-enabled using the Change method.
-        /// </remarks>
+        /// </summary>
+        /// <param name="callback">A TimerCallback delegate representing a method to be executed.</param>
         [Template("new Bridge.Threading.Timer(\"constructor\", {callback}, {state}, {dueTime}, {period})")]
         public extern Timer(TimerCallback callback);
 
