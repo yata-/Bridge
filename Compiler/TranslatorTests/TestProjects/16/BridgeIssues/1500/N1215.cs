5a9cbffe0a8499e50ae3f79c3a841b70bcfceb44
LeonidVeriga <leonid@object.net>
2016-04-19 15:56:34 +0300

    [TEST] [#1215] Compiler test

---
+++ Compiler/TranslatorTests/TestProjects/16/BridgeIssues/1500/N1215.cs
@@ -0,0 +1,34 @@
+﻿//#1215
+namespace Test.BridgeIssues.N1215
+{
+    public class TestExtraBrackets
+    {
+        public void TestDouble()
+        {
+            double v = 0;
+            v = --v;
+            v = ++v;
+        }
+
+        public void TestDecimal()
+        {
+            decimal v = 0;
+            v = --v;
+            v = ++v;
+        }
+
+        public void TestSingle()
+        {
+            float v = 0;
+            v = --v;
+            v = ++v;
+        }
+
+        public void TestLong()
+        {
+            long v = 0;
+            v = --v;
+            v = ++v;
+        }
+    }
+}
