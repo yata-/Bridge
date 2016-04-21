e3ce7797c88e88457135136b339a146508cd5e3d
LeonidVeriga <leonid@object.net>
2016-04-22 00:43:09 +0300

    [#1230] Additional Compiler tests

--- Compiler/TranslatorTests/TestProjects/18/Bridge/reference/testIssue1230.js
+++ Compiler/TranslatorTests/TestProjects/18/Bridge/reference/testIssue1230.js
@@ -3,7 +3,8 @@
 
     Bridge.define('TestIssue1230.Issue1230', {
         statics: {
-            test: function () {
+            testLong: function () {
+                // Conversions should not have duplicated Bridge.Long: Bridge.Long(Bridge.Long(v))
                 var v = 7;
                 var l = Bridge.Long(v);
                 l = Bridge.Long(v);
@@ -13,10 +14,41 @@
                 console.log(Bridge.Long(2).toString());
                 console.log(Bridge.Long(2).toString());
                 console.log(Bridge.Long(7).toString());
-                TestIssue1230.Issue1230.method(Bridge.Long(v));
-                TestIssue1230.Issue1230.method(Bridge.Long(v));
+                TestIssue1230.Issue1230.methodLong(Bridge.Long(v));
+                TestIssue1230.Issue1230.methodLong(Bridge.Long(v));
             },
-            method: function (l) {
+            methodLong: function (l) {
+            },
+            methodDecimal: function (l) {
+            },
+            methodInt: function (l) {
+            },
+            testDecimal: function () {
+                // Conversions should not have duplicated Bridge.Decimal: Bridge.Decimal(Bridge.Decimal(v))
+                var v = 7;
+                var l = Bridge.Decimal(v);
+                l = Bridge.Decimal(v);
+    
+                console.log(Bridge.Decimal(v).toString());
+                console.log(Bridge.Decimal((v)).toString());
+                console.log(Bridge.Decimal(2.0).toString());
+                console.log(Bridge.Decimal(2.0).toString());
+                console.log(Bridge.Decimal(7.0).toString());
+                TestIssue1230.Issue1230.methodDecimal(Bridge.Decimal(v));
+                TestIssue1230.Issue1230.methodDecimal(Bridge.Decimal(v));
+            },
+            testInt: function () {
+                var v = 7;
+                var l = v;
+                l = v;
+    
+                console.log(v);
+                console.log((v));
+                console.log(2);
+                console.log(2);
+                console.log(7);
+                TestIssue1230.Issue1230.methodInt(v);
+                TestIssue1230.Issue1230.methodInt(v);
             }
         }
     });
--- Compiler/TranslatorTests/TestProjects/18/Issues/Issue1230.cs
+++ Compiler/TranslatorTests/TestProjects/18/Issues/Issue1230.cs
@@ -1,12 +1,12 @@
-﻿using Bridge;
-using Bridge.Html5;
+﻿using Bridge.Html5;
 
 namespace TestIssue1230
 {
     public class Issue1230
     {
-        public static void Test()
+        public static void TestLong()
         {
+            // Conversions should not have duplicated Bridge.Long: Bridge.Long(Bridge.Long(v))
             int v = 7;
             long l = (long)v;
             l = v;
@@ -16,12 +16,51 @@ namespace TestIssue1230
             Console.Log((long)(1 + 1));
             Console.Log((long)(2));
             Console.Log((long)7);
-            Method(v);
-            Method((long)v);
+            MethodLong(v);
+            MethodLong((long)v);
         }
 
-        public static void Method(long l)
+        public static void MethodLong(long l)
         {
+        }
+
+        public static void MethodDecimal(decimal l)
+        {
+        }
+
+        public static void MethodInt(int l)
+        {
+        }
+
+        public static void TestDecimal()
+        {
+            // Conversions should not have duplicated Bridge.Decimal: Bridge.Decimal(Bridge.Decimal(v))
+            int v = 7;
+            decimal l = (decimal)v;
+            l = v;
+
+            Console.Log((decimal)v);
+            Console.Log((decimal)(v));
+            Console.Log((decimal)(1 + 1));
+            Console.Log((decimal)(2));
+            Console.Log((decimal)7);
+            MethodDecimal(v);
+            MethodDecimal((decimal)v);
+        }
+
+        public static void TestInt()
+        {
+            byte v = 7;
+            int l = (int)v;
+            l = v;
+
+            Console.Log((int)v);
+            Console.Log((int)(v));
+            Console.Log((int)(1 + 1));
+            Console.Log((int)(2));
+            Console.Log((int)7);
+            MethodInt(v);
+            MethodInt((int)v);
         }
     }
 }
