4a1c46ff34ce4a77446c6d9d781a57a031aa1cf4
Geoffrey McGill <geoff@object.net>
2016-05-01 00:02:49 -0600

    [#1042] Remove scripts that forced reload of csproj

--- .build/files/install.ps1
+++ .build/files/install.ps1
@@ -22,114 +22,35 @@ ForEach ($item in $project.Object.References)
 }
 
 # Sets the NoStdLib setting to True for every project configuration.
-# See Issue #419 for more information on why.
-# https://github.com/bridgedotnet/Bridge/issues/419
-# Once Visual Studio or NuGet defect is fixed, lines 24-133 can be removed.
+$project.ConfigurationManager | ForEach-Object {
 
-$projectMSBuild = [Microsoft.Build.Construction.ProjectRootElement]::Open($project.FullName)
+  # Check for <NoStdLib>
+  $nostdlib_setting = $_.Properties.Item("NoStdLib")
 
-if ($projectMSBuild)
-{
-    # Clean all NoStdLib, AddAdditionalExplicitAssemblyReferences and AdditionalExplicitAssemblyReferences to avoid duplicates
-    # Check if the project already has all the required properties - it means no Save&Reload dialog required
-    $effectiveStd = $false
-    $effectiveAAEAR = $false
-    $effectiveAEAR = $false
+  if (-not $nostdlib_setting.Value) 
+  {
+    $nostdlib_setting.Value = $true
+  }
 
-    $wasModified = $false
 
-    ForEach ($item in $projectMSBuild.Properties)
-    {
-        $name = $item.Name
-        if ($name -ieq "NoStdLib" -or $name -ieq "AddAdditionalExplicitAssemblyReferences" -or $name -ieq "AdditionalExplicitAssemblyReferences")
-        {
-            #Write-Host ($item.Name + ":" + $item.Value + " Condition:" + $item.Condition  + " Parent Condition:" + $item.Parent.Condition)
+  # Check for <AddAdditionalExplicitAssemblyReferences>
+  #$addadditionalexplicitassemblyreferences_setting = $_.Properties.Item("AddAdditionalExplicitAssemblyReferences")
 
-            # Consider only properties with no conditions
-            $condition = !($item.Condition) -and !($item.Parent.Condition)
+  #if (-not $addadditionalexplicitassemblyreferences_setting.Value) 
+  #{
+  #  $addadditionalexplicitassemblyreferences_setting.Value = $false
+  #}
 
-            $isEffective = $false
 
-            # The required property does not have condition i.e. applied everytime
-            # Then check if the property has required value
-            # The last one (as there may be several similar properties) matters
-            switch ($name)
-            {
-                "NoStdLib"
-                { 
-                    $effectiveStd = $condition -and ($item.Value -ieq "true");
-                    $isEffective =  $effectiveStd;
-                } 
-                "AddAdditionalExplicitAssemblyReferences"
-                {
-                    $effectiveAAEAR = $condition -and ($item.Value -ieq "false");
-                    $isEffective =  $effectiveAAEAR;
-                } 
-                "AdditionalExplicitAssemblyReferences"
-                {
-                    $effectiveAEAR = $condition -and (($item.Value -eq $null) -or ($item.Value -eq ""));
-                    $isEffective =  $effectiveAEAR;
-                } 
-            }
+  # Check for <AdditionalExplicitAssemblyReferences>
+  #$additionalexplicitassemblyreferences_setting = $_.Properties.Item("AdditionalExplicitAssemblyReferences")
 
-            # Remove the property if it is not appropriate - conditional or having wrong value
-            if (!$isEffective)
-            {
-                Try
-                {
-                    $item.Parent.RemoveChild($item);
-                    $wasModified = $true;
-                    Write-Host ("Removed Property " + $name)
-                }
-                Catch
-                {
-                    Write-Host ("Failed to remove Property " + $name + "Error: " +  $_.Exception.Message)
-                }
-            }
-        }
-    }
+  #if (-not $additionalexplicitassemblyreferences_setting.Value) 
+  #{
+  #  Write-Host ("AdditionalExplicitAssemblyReferences")
+  #  # $additionalexplicitassemblyreferences_setting.Value = $true
+  #}
 
-    if (!$effectiveStd)
-    {
-        $projectMSBuild.AddProperty('NoStdLib', 'true');
-        Write-Host ("Added Property NoStdLib")
-        $wasModified = $true;
-    }
-
-    if (!$effectiveAAEAR)
-    {
-        $projectMSBuild.AddProperty('AddAdditionalExplicitAssemblyReferences', 'false');
-        Write-Host ("Added Property AddAdditionalExplicitAssemblyReferences")
-        $wasModified = $true;
-    }
-
-    if (!$effectiveAEAR)
-    {
-        $propEnableNuGetImport = $projectMSBuild.AddProperty('AdditionalExplicitAssemblyReferences', $null);
-        Write-Host ("Added Property AdditionalExplicitAssemblyReferences")        
-        $wasModified = $true;
-    }
-
-    if ($wasModified)
-    {
-        $project.Save();
-        Write-Host ("Saved the project as <NoStdLib>, <AddAdditionalExplicitAssemblyReferences> or <AdditionalExplicitAssemblyReferences> properties adjusted")
-    }
-    
-    if ($effectiveStd -and $effectiveAAEAR -and $effectiveAEAR)
-    {
-        Write-Host ("Reloading the project is not required")
-    }
-    else
-    {
-        $p = $(get-item $project.FullName)
-        if ($p)
-        {
-            Write-Host $p.lastwritetime
-            $p.lastwritetime=get-date
-            Write-Host ("Triggered project reloading as it is required to fix possible IntelliSense errors")
-        }
-    }
 }
 
 Write-Host ("Bridge.NET installation was successful")
