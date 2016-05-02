a995835ba0c9c0f9f93ce68dfe3a0593e9590e83
Geoffrey McGill <geoff@object.net>
2016-05-01 22:14:28 -0600

    Added NuGet publishing command for Bridge.Core and Bridge.Min

--- .build/packages/publish.bat
+++ .build/packages/publish.bat
@@ -1,14 +1,23 @@
 @ECHO OFF
 
-set key1=Bridge
-
 SET /p proceed=Are you sure? [y] or [n]
 SET /p apikey=NuGet API Key?
 SET /p version=Release version number?
 
 IF "%proceed%"=="y" (
 	:: This process may take a long time to run
-	..\..\.nuget\NuGet.exe push %key1%.%version%.nupkg  %apikey%
+	..\..\.nuget\NuGet.exe push Bridge.%version%.nupkg  %apikey%
+
+	ECHO Bridge has been published
+
+	..\..\.nuget\NuGet.exe push Bridge.Core.%version%.nupkg  %apikey%
+	
+	ECHO Bridge.Core has been published
+
+	..\..\.nuget\NuGet.exe push Bridge.Min.%version%.nupkg  %apikey%
+	
+	ECHO Bridge.Min has been published
+
 	ECHO SUCCESS!
 )
 
