3285f2d9af5ccf174d21ae93854d627b8522938a
Geoffrey McGill <geoff@object.net>
2016-05-03 14:26:18 -0600

    Update NuGet package deployment items

--- .build/packages/publish.bat
+++ .build/packages/publish.bat
@@ -5,20 +5,28 @@ SET /p apikey=NuGet API Key?
 SET /p version=Release version number?
 
 IF "%proceed%"=="y" (
-	:: This process may take a long time to run
-	..\..\.nuget\NuGet.exe push Bridge.%version%.nupkg  %apikey%
+    :: This process may take a long time to run
+    ..\..\.nuget\NuGet.exe push Bridge.Core.%version%.nupkg  %apikey%
+    
+    ECHO Bridge.Core has been published
 
-	ECHO Bridge has been published
 
-	..\..\.nuget\NuGet.exe push Bridge.Core.%version%.nupkg  %apikey%
-	
-	ECHO Bridge.Core has been published
+    ..\..\.nuget\NuGet.exe push Bridge.Min.%version%.nupkg  %apikey%
+    
+    ECHO Bridge.Min has been published
 
-	..\..\.nuget\NuGet.exe push Bridge.Min.%version%.nupkg  %apikey%
-	
-	ECHO Bridge.Min has been published
 
-	ECHO SUCCESS!
+    ..\..\.nuget\NuGet.exe push Bridge.Html5.%version%.nupkg  %apikey%
+
+    ECHO Bridge.Html5 has been published
+
+
+    ..\..\.nuget\NuGet.exe push Bridge.%version%.nupkg  %apikey%
+
+    ECHO Bridge has been published
+
+
+    ECHO SUCCESS!
 )
 
 PAUSE
