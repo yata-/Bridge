b415e9c8542d0755b19ea7b42c8ea6b2469e3813
LeonidVeriga <leonid@object.net>
2016-05-11 16:18:20 +0300

    [#1304] Change condition to output into internal buffer

--- Bridge/Resources/Console.js
+++ Bridge/Resources/Console.js
@@ -1,7 +1,7 @@
     // @source Console.js    Bridge.Console = {
         output: null,        log: function (obj) {
-            if (!Bridge.Console.output) {
+            if (Bridge.Console.output != null) {
                 Bridge.Console.output += obj;                return;
             }            console.log(obj);
-        },
+        }
     };
