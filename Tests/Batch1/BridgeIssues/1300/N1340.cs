99ad177c1ec1e6f85f416b088b05c20ffe21d24c
LeonidVeriga <leonid@object.net>
2016-05-16 00:58:29 +0300

    [TEST] [#1340] Client tests for generic static fields

--- Tests/Batch1/BridgeIssues/1300/N1340.cs
+++ Tests/Batch1/BridgeIssues/1300/N1340.cs
@@ -170,7 +170,7 @@ namespace Bridge.ClientTest.BridgeIssues
         }
 
         [Test]
-        public static void TestArray()
+        public static void TestStructStaticIntArray()
         {
             Assert.AreEqual(0, Data<int>.array[0]);
             Assert.AreEqual(0, Data<int>.array[1]);
@@ -186,6 +186,50 @@ namespace Bridge.ClientTest.BridgeIssues
             Assert.AreEqual(0, o.ReturnArray()[2]);
 
             var o1 = new Data<Data<int>>();
+            Assert.NotNull(o1.ReturnArray()[0]);
+            Assert.NotNull(o1.ReturnArray()[1]);
+            Assert.NotNull(o1.ReturnArray()[2]);
+        }
+
+        [Test]
+        public static void TestStructStaticLongArray()
+        {
+            Assert.AreEqual(0L, Data<long>.array[0]);
+            Assert.AreEqual(0L, Data<long>.array[1]);
+            Assert.AreEqual(0L, Data<long>.array[2]);
+
+            Assert.NotNull(Data<Data<long>>.array[0]);
+            Assert.NotNull(Data<Data<long>>.array[1]);
+            Assert.NotNull(Data<Data<long>>.array[2]);
+
+            var o = new Data<long>();
+            Assert.AreEqual(0L, o.ReturnArray()[0]);
+            Assert.AreEqual(0L, o.ReturnArray()[1]);
+            Assert.AreEqual(0L, o.ReturnArray()[2]);
+
+            var o1 = new Data<Data<long>>();
+            Assert.NotNull(o1.ReturnArray()[0]);
+            Assert.NotNull(o1.ReturnArray()[1]);
+            Assert.NotNull(o1.ReturnArray()[2]);
+        }
+
+        [Test]
+        public static void TestStructStaticStringArray()
+        {
+            Assert.Null(Data<string>.array[0]);
+            Assert.Null(Data<string>.array[1]);
+            Assert.Null(Data<string>.array[2]);
+
+            Assert.NotNull(Data<Data<string>>.array[0]);
+            Assert.NotNull(Data<Data<string>>.array[1]);
+            Assert.NotNull(Data<Data<string>>.array[2]);
+
+            var o = new Data<string>();
+            Assert.Null(o.ReturnArray()[0]);
+            Assert.Null(o.ReturnArray()[1]);
+            Assert.Null(o.ReturnArray()[2]);
+
+            var o1 = new Data<Data<string>>();
             Assert.NotNull(o1.ReturnArray()[0]);
             Assert.NotNull(o1.ReturnArray()[1]);
             Assert.NotNull(o1.ReturnArray()[2]);
@@ -242,6 +286,105 @@ namespace Bridge.ClientTest.BridgeIssues
         {
             public T Value3 { get; set; }
             public K Value4;
+        }
+
+        [Test]
+        public static void TestStaticClassGenericMembersDefaultValue()
+        {
+            Assert.AreEqual(0, StaticDataClass<int>.Value1, "int 1");
+            Assert.AreEqual(0, StaticDataClass<int>.Value2, "int 2");
+
+            Assert.AreEqual(0m, StaticDataClass<decimal>.Value1, "decimal 1");
+            Assert.AreEqual(0m, StaticDataClass<decimal>.Value2, "decimal 2");
+
+            Assert.AreEqual(0L, StaticDataClass<long>.Value1, "long 1");
+            Assert.AreEqual(0L, StaticDataClass<long>.Value2, "long 2");
+
+            Assert.AreEqual(false, StaticDataClass<bool>.Value1, "bool 1");
+            Assert.AreEqual(false, StaticDataClass<bool>.Value2, "bool 2");
+
+            Assert.Null(StaticDataClass<string>.Value1, "string 1");
+            Assert.Null(StaticDataClass<string>.Value2, "string 2");
+
+            Assert.NotNull(StaticDataClass<Data<int>>.Value1, "Data<int> 1");
+            Assert.AreEqual(0, StaticDataClass<Data<int>>.Value1.Value1, "Data<int>.Value1 1");
+            Assert.AreEqual(0, StaticDataClass<Data<int>>.Value1.Value2, "Data<int>.Value1 2");
+            Assert.NotNull(StaticDataClass<Data<int>>.Value2, "Data<int> 2");
+            Assert.AreEqual(0, StaticDataClass<Data<int>>.Value2.Value1, "Data<int>.Value2 1");
+            Assert.AreEqual(0, StaticDataClass<Data<int>>.Value2.Value2, "Data<int>.Value2 2");
+
+            Assert.Null(StaticDataClass<DataClass<int>>.Value1, "DataClass<int> 1");
+            Assert.Null(StaticDataClass<DataClass<int>>.Value2, "DataClass<int> 2");
+        }
+
+        [Test]
+        public static void TestStaticClassTwoGenericMembersDefaultValue()
+        {
+            Assert.AreEqual(0, StaticDataClass<int, decimal>.Value1, "int 1");
+            Assert.AreEqual(0m, StaticDataClass<int, decimal>.Value2, "decimal 2");
+
+            Assert.AreEqual(0L, StaticDataClass<long, bool>.Value1, "long 1");
+            Assert.AreEqual(false, StaticDataClass<long, bool>.Value2, "bool 2");
+
+            Assert.AreEqual(0, StaticDataClass<double, string>.Value1, "double 1");
+            Assert.Null(StaticDataClass<double, string>.Value2, "string 2");
+        }
+
+        [Test]
+        public static void TestStaticClass1TwoGenericInheritedMembersDefaultValue()
+        {
+            Assert.AreEqual(0, StaticDataClass1<int, decimal>.Value1, "int 1");
+            Assert.AreEqual(0m, StaticDataClass1<int, decimal>.Value2, "decimal 2");
+
+            Assert.AreEqual(0L, StaticDataClass1<long, bool>.Value1, "long 1");
+            Assert.AreEqual(false, StaticDataClass1<long, bool>.Value2, "bool 2");
+
+            Assert.AreEqual(0, StaticDataClass1<double, string>.Value1, "double 1");
+            Assert.Null(StaticDataClass1<double, string>.Value2, "string 2");
+        }
+
+        [Test]
+        public static void TestStaticClass2TwoGenericInheritedMembersDefaultValue()
+        {
+            Assert.AreEqual(0, StaticDataClass2<decimal, bool>.Value1, "int 1");
+            Assert.Null(StaticDataClass2<decimal, bool>.Value2, "string 2");
+        }
+
+        [Test]
+        public static void TestStaticClass3TwoGenericInheritedMembersDefaultValue()
+        {
+            Assert.AreEqual(0, StaticDataClass3<long, decimal>.Value1, "int 1");
+            Assert.Null(StaticDataClass3<long, decimal>.Value2, "string 2");
+            Assert.AreEqual(0L, StaticDataClass3<long, decimal>.Value3, "long 1");
+            Assert.AreEqual(0m, StaticDataClass3<long, decimal>.Value4, "decimal 4");
+        }
+
+        public class StaticDataClass<T>
+        {
+            public static T Value1 { get; set; }
+            public static T Value2;
+        }
+
+        public class StaticDataClass<T, K>
+        {
+            public static T Value1 { get; set; }
+            public static K Value2;
+        }
+
+        public class StaticDataClass1<T, K> : StaticDataClass<T, K>
+        {
+
+        }
+
+        public class StaticDataClass2<T, K> : StaticDataClass1<int, string>
+        {
+
+        }
+
+        public class StaticDataClass3<T, K> : StaticDataClass2<int, string>
+        {
+            public static T Value3 { get; set; }
+            public static K Value4;
         }
     }
 }
