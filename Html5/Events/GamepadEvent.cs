5d4e57f94ca0db36fa84cebe04f8390cfb2ad3b0
LeonidVeriga <leonid@object.net>
2016-05-16 15:24:23 +0300

    Upper case field names in GamepadButton and GamepadEvent

--- Html5/Events/GamepadEvent.cs
+++ Html5/Events/GamepadEvent.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Bridge.Html5
+﻿namespace Bridge.Html5
 {
     [External]
     [Name(false)]
     public class GamepadEvent : Event
     {
-        public readonly Gamepad gamepad;
+        public readonly Gamepad Gamepad;
     }
 }
