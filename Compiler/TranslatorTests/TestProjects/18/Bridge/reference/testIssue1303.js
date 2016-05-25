67f3624e9c3048983debcc511de67b47efd28a62
LeonidVeriga <leonid@object.net>
2016-05-26 01:39:45 +0300

    [TEST] Compiler test to check `static Main` method is AutoSetupMethod

---
+++ Compiler/TranslatorTests/TestProjects/18/Bridge/reference/testIssue1303.js
@@ -0,0 +1,54 @@
+﻿(function (globals) {
+    "use strict";
+
+    Bridge.define('TestIssue1303.App', {
+        statics: {
+            config: {
+                init: function () {
+                    Bridge.ready(this.notMain);
+                    Bridge.ready(this.main);
+                }
+            },
+            notMain: function () {
+                //Should be in config.init
+            },
+            main: function () {
+                //Should be in config.init
+            }
+        }
+    });
+    
+    Bridge.define('TestIssue1303.App1', {
+        statics: {
+            config: {
+                init: function () {
+                    Bridge.ready(this.notMain);
+                    Bridge.ready(this.main)
+                }
+            },
+            notMain: function () {
+                //Should be in config.init
+            },
+            main: function () {
+                //Should be in config.init
+            }
+        }
+    });
+    
+    Bridge.define('TestIssue1303.App2', {
+        statics: {
+            config: {
+                init: function () {
+                    Bridge.ready(this.main);
+                }
+            },
+            main: function () {
+                //Should be in config.init
+            }
+        }
+    });
+    
+    
+    
+    Bridge.init();
+})(this);
