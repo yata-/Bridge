3434ccbf6f1abc87d0b0eccb277e7c44f03488de
LeonidVeriga <leonid@object.net>
2016-05-27 23:21:31 +0300

    [#1369] [#1404] Version and RegexMatchTimeoutException should not have [External] as transpiled

--- Bridge/System/RegexMatchTimeoutException.cs
+++ Bridge/System/RegexMatchTimeoutException.cs
@@ -1,14 +1,11 @@
-﻿using Bridge;
-
-namespace System
+﻿namespace System
 {
-    [External]
+    //[External] the class should have External attribute as it uses transpiled js code Version.js
     public class RegexMatchTimeoutException : TimeoutException
     {
         public extern string Pattern { get; }
         public extern string Input { get; }
         public extern TimeSpan MatchTimeout { get; }
-
 
         public extern RegexMatchTimeoutException();
 
