9c41483ce1b967f1d6817111be82f76e48598892
Michael Cheers <michaelcheers8@gmail.com>
2016-05-27 08:04:36 +0200

    Separated all attributes including non-separated before.

--- Bridge/System/Action.cs
+++ Bridge/System/Action.cs
@@ -3,39 +3,57 @@ using Bridge;
 namespace System
 {
     [Name("Function")]
-    [IgnoreCast, IgnoreGeneric, External]
+    [IgnoreCast]
+	[IgnoreGeneric]
+	[External]
     public delegate void Action();
 
     [Name("Function")]
-    [IgnoreCast, IgnoreGeneric, External]
+    [IgnoreCast]
+	[IgnoreGeneric]
+	[External]
     public delegate void Action<T>(T arg);
 
     [Name("Function")]
-    [IgnoreCast, IgnoreGeneric, External]
+    [IgnoreCast]
+	[IgnoreGeneric]
+	[External]
     public delegate void Action<T1, T2>(T1 arg1, T2 arg2);
 
     [Name("Function")]
-    [IgnoreCast, IgnoreGeneric, External]
+    [IgnoreCast]
+	[IgnoreGeneric]
+	[External]
     public delegate void Action<T1, T2, T3>(T1 arg1, T2 arg2, T3 arg3);
 
     [Name("Function")]
-    [IgnoreCast, IgnoreGeneric, External]
+    [IgnoreCast]
+	[IgnoreGeneric]
+	[External]
     public delegate void Action<T1, T2, T3, T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
 
     [Name("Function")]
-    [IgnoreCast, IgnoreGeneric, External]
+    [IgnoreCast]
+	[IgnoreGeneric]
+	[External]
     public delegate void Action<T1, T2, T3, T4, T5>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
 
     [Name("Function")]
-    [IgnoreCast, IgnoreGeneric, External]
+    [IgnoreCast]
+	[IgnoreGeneric]
+	[External]
     public delegate void Action<T1, T2, T3, T4, T5, T6>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
 
     [Name("Function")]
-    [IgnoreCast, IgnoreGeneric, External]
+    [IgnoreCast]
+	[IgnoreGeneric]
+	[External]
     public delegate void Action<T1, T2, T3, T4, T5, T6, T7>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
 
     [Name("Function")]
-    [IgnoreCast, IgnoreGeneric, External]
+    [IgnoreCast]
+	[IgnoreGeneric]
+	[External]
     public delegate void Action<T1, T2, T3, T4, T5, T6, T7, T8>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8);
 
     [Name("Function")]
