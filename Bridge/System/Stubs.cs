875f78a864a6cfb82fb280c4bbcdd02cc51e3479
Vladimir Angelov <vladimir@siriushome.com>
2016-05-29 00:50:35 +0300

    Implemented Type.IsAssignableFrom.

--- Bridge/System/Stubs.cs
+++ Bridge/System/Stubs.cs
@@ -19,6 +19,8 @@ namespace System
         }
 
         public static extern Type GetTypeFromHandle(RuntimeTypeHandle typeHandle);
+
+        public extern bool IsAssignableFrom(Type type);
     }
 
     [External]
