14905b6fa11fd63ea0ed49300734778d69b15c72
Geoffrey McGill <geoff@object.net>
2016-07-30 11:37:26 -0600

    Sync TypeScript runner .js files

--- Tests/Runner/TypeScript/Batch1/4_Functions.js
+++ Tests/Runner/TypeScript/Batch1/4_Functions.js
@@ -1,10 +1,9 @@
-﻿/// <reference path="..\..\Runner\resources\qunit\qunit.d.ts" />
+/// <reference path="..\..\Runner\resources\qunit\qunit.d.ts" />
 /// <reference path="..\..\Runner\TypeScript\App1\bridge.d.ts" />
 /// <reference path="..\..\Runner\TypeScript\App1\functions.d.ts" />
 QUnit.module("TypeScript - Functions");
 QUnit.test("Parameters", function (assert) {
     var func = new Functions.Parameters();
-
     // TODO Bridge/#292
     // assert.deepEqual(func.getSomething(), 5, "Default parameter #292");
     //function buildName(firstName: string, lastName = "Smith") {
@@ -16,27 +15,16 @@ QUnit.test("Parameters", function (assert) {
     // #293
     assert.deepEqual(func.join([1, 2, 3]), "123", "params argument becomes Array #293");
 });
-
 QUnit.test("Function types", function (assert) {
     var d = new Functions.DelegateClass();
-
     var ds;
     var di;
-
-    d.methodVoidDelegate = function () {
-        return di = 7;
-    };
+    d.methodVoidDelegate = function () { return di = 7; };
     d.methodVoidDelegate();
     assert.deepEqual(di, 7, "methodVoidDelegate");
-
-    d.methodStringDelegate = function (s) {
-        return ds = s;
-    };
+    d.methodStringDelegate = function (s) { return ds = s; };
     d.methodStringDelegate("Privet");
     assert.deepEqual(ds, "Privet", "methodStringDelegate");
-
-    d.methodStringDelegateIntResult = function (s) {
-        return s.length;
-    };
+    d.methodStringDelegateIntResult = function (s) { return s.length; };
     assert.deepEqual(d.methodStringDelegateIntResult("Privet"), 6, "methodStringDelegateIntResult");
 });
