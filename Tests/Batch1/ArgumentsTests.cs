621c2ab22ea07d8cce1d01e4f5d0fee9c0000a38
LeonidVeriga <leonid@object.net>
2016-08-04 01:58:47 +0300

    [#1561] C# code formatting

--- Tests/Batch1/ArgumentsTests.cs
+++ Tests/Batch1/ArgumentsTests.cs
@@ -7,74 +7,74 @@ namespace Bridge.ClientTest
 {
     [Category(Constants.MODULE_ARGUMENTS)]
     [TestFixture]
-	public class ArgumentsTests
+    public class ArgumentsTests
     {
-		[ExpandParams]
-		private void LengthHelper0(params object[] args)
+        [ExpandParams]
+        private void LengthHelper0(params object[] args)
         {
-			Assert.AreEqual(Arguments.Length, 0);
-		}
+            Assert.AreEqual(Arguments.Length, 0);
+        }
 
-		[ExpandParams]
-		private void LengthHelper1(params object[] args)
+        [ExpandParams]
+        private void LengthHelper1(params object[] args)
         {
-			Assert.AreEqual(Arguments.Length, 1);
-		}
+            Assert.AreEqual(Arguments.Length, 1);
+        }
 
-		[ExpandParams]
-		private void LengthHelper2(params object[] args)
+        [ExpandParams]
+        private void LengthHelper2(params object[] args)
         {
-			Assert.AreEqual(Arguments.Length, 2);
-		}
+            Assert.AreEqual(Arguments.Length, 2);
+        }
 
-		[ExpandParams]
-		private object GetArgumentHelper(int index, params object[] args)
+        [ExpandParams]
+        private object GetArgumentHelper(int index, params object[] args)
         {
-			return Arguments.GetArgument(index);
-		}
+            return Arguments.GetArgument(index);
+        }
 
-		[ExpandParams]
-		private object ToArrayHelper(params object[] args)
+        [ExpandParams]
+        private object ToArrayHelper(params object[] args)
         {
-			return Arguments.ToArray();
-		}
+            return Arguments.ToArray();
+        }
 
-		[ExpandParams]
-		private object ToArrayHelper<T>(params object[] args)
+        [ExpandParams]
+        private object ToArrayHelper<T>(params object[] args)
         {
-			return Arguments.ToArray<T>(1); // first argument will be generic type
-		}
+            return Arguments.ToArray<T>(1); // first argument will be generic type
+        }
 
-		[Test]
-		public void LengthWorks()
+        [Test]
+        public void LengthWorks()
         {
-			LengthHelper0();
-			LengthHelper1(4);
-			LengthHelper2(6, "x");
-		}
+            LengthHelper0();
+            LengthHelper1(4);
+            LengthHelper2(6, "x");
+        }
 
-		[Test]
-		public void GetArgumentWorks()
+        [Test]
+        public void GetArgumentWorks()
         {
-			Assert.AreEqual(GetArgumentHelper(0, "x", "y"), 0);
-			Assert.AreEqual(GetArgumentHelper(1, "x", "y"), "x");
-			Assert.AreEqual(GetArgumentHelper(2, "x", "y"), "y");
-		}
+            Assert.AreEqual(GetArgumentHelper(0, "x", "y"), 0);
+            Assert.AreEqual(GetArgumentHelper(1, "x", "y"), "x");
+            Assert.AreEqual(GetArgumentHelper(2, "x", "y"), "y");
+        }
 
-		[Test]
-		public void ToArrayWorks()
+        [Test]
+        public void ToArrayWorks()
         {
-			Assert.AreEqual(ToArrayHelper(), new object[0]);
-			Assert.AreEqual(ToArrayHelper("x"), new object[] { "x" });
-			Assert.AreEqual(ToArrayHelper("x", 1), new object[] { "x", 1 });
-		}
+            Assert.AreEqual(ToArrayHelper(), new object[0]);
+            Assert.AreEqual(ToArrayHelper("x"), new object[] { "x" });
+            Assert.AreEqual(ToArrayHelper("x", 1), new object[] { "x", 1 });
+        }
 
-		[Test]
-		public void ToArrayOfTWorks()
+        [Test]
+        public void ToArrayOfTWorks()
         {
-			Assert.AreEqual(ToArrayHelper<string>(), new object[0]);
-			Assert.AreEqual(ToArrayHelper<string>("x"), new[] { "x" });
-			Assert.AreEqual(ToArrayHelper<string>("x", "y"), new[] { "x", "y" });
-		}
-	}
+            Assert.AreEqual(ToArrayHelper<string>(), new object[0]);
+            Assert.AreEqual(ToArrayHelper<string>("x"), new[] { "x" });
+            Assert.AreEqual(ToArrayHelper<string>("x", "y"), new[] { "x", "y" });
+        }
+    }
 }
