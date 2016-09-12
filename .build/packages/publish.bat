e6cf00e9cf044b929a31973981705813d0b6e686
Geoffrey McGill <geoff@object.net>
2016-09-12 12:44:45 -0600

    Update NuGet package version numbers

--- .build/packages/publish.bat
+++ .build/packages/publish.bat
@@ -36,27 +36,27 @@ SET /p proceed2=Have you updated the framework .nuspec version numbers? [y] or [
 IF "%proceed2%"=="y" (
     ECHO Starting Framework Publishing...
 
-    ..\..\.nuget\NuGet.exe push Bridge.Bootstrap.3.9.1.nupkg  %apikey%
+    ..\..\.nuget\NuGet.exe push Bridge.Bootstrap.3.9.3.nupkg  %apikey%
     ECHO Bridge.Bootstrap has been published
 
 
-    ..\..\.nuget\NuGet.exe push Bridge.Collections.1.3.0.nupkg  %apikey%
+    ..\..\.nuget\NuGet.exe push Bridge.Collections.1.3.2.nupkg  %apikey%
     ECHO Bridge.Collections has been published
 
 
-    ..\..\.nuget\NuGet.exe push Bridge.Html5.Console.1.0.2.nupkg  %apikey%
-    ECHO Bridge.Html5.Console has been published
+    ::..\..\.nuget\NuGet.exe push Bridge.Html5.Console.1.0.2.nupkg  %apikey%
+    ::ECHO Bridge.Html5.Console has been published
 
 
-    ..\..\.nuget\NuGet.exe push Bridge.jQuery.2.9.0.nupkg  %apikey%
+    ..\..\.nuget\NuGet.exe push Bridge.jQuery.2.9.2.nupkg  %apikey%
     ECHO Bridge.jQuery has been published
 
 
-    ..\..\.nuget\NuGet.exe push Bridge.QUnit.1.8.0.nupkg  %apikey%
+    ..\..\.nuget\NuGet.exe push Bridge.QUnit.1.8.2.nupkg  %apikey%
     ECHO Bridge.QUnit has been published
 
 
-    ..\..\.nuget\NuGet.exe push Bridge.WebGL.1.8.0.nupkg  %apikey%
+    ..\..\.nuget\NuGet.exe push Bridge.WebGL.1.8.2.nupkg  %apikey%
     ECHO Bridge.WebGL has been published
 
 
