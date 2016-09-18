b627f6816302c9943539b8fc22b7070d974bcda9
Geoffrey McGill <geoff@object.net>
2016-09-18 14:45:55 -0600

    Update Assembly Version to Bridge 15.1.0

--- Bridge/Resources/.auxiliary/Header.js
+++ Bridge/Resources/.auxiliary/Header.js
@@ -1,7 +1,7 @@
 ﻿/*
  * @version   : {version} - Bridge.NET
  * @author    : Object.NET, Inc. http://bridge.net/
- * @date      : 2016-09-12
+ * @date      : 2016-09-19
  * @copyright : Copyright 2008-{year} Object.NET, Inc. http://object.net/
  * @license   : See license.txt and https://github.com/bridgedotnet/Bridge.NET/blob/master/LICENSE.
 */
