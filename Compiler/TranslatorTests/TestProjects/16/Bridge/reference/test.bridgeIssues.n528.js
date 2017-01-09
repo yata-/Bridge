230f9524f3017d38aa7e235e4dcca68447a18c6b
LeonidVeriga <leonid@object.net>
2017-01-09 20:00:06 +0300

    [2217] Update Compiler Test 16. TODO: Test 07 [skip ci]

--- Compiler/TranslatorTests/TestProjects/16/Bridge/reference/test.bridgeIssues.n528.js
+++ Compiler/TranslatorTests/TestProjects/16/Bridge/reference/test.bridgeIssues.n528.js
@@ -1,28 +1,28 @@
 Bridge.assembly("TestProject", function ($asm, globals) {
     "use strict";
 
-define("Bridge528_A", ["bridge","dep1"], function (_, dep1) {
-    var exports = { };
-        Bridge.define("Test.BridgeIssues.N528.Bridge528_A", {
-            $scope: exports
-        });
-    return exports;
-});
+    define("Bridge528_A", ["dep1"], function (dep1) {
+        var Bridge528_A = { };
+            Bridge.define("Test.BridgeIssues.N528.Bridge528_A", {
+                $scope: Bridge528_A
+            });
+        return Bridge528_A;
+    });
 
-define("Bridge528_B", ["bridge","dep2"], function (_, dep2) {
-    var exports = { };
-        Bridge.define("Test.BridgeIssues.N528.Bridge528_B", {
-            $scope: exports
-        });
-    return exports;
-});
+    define("Bridge528_B", ["dep2"], function (dep2) {
+        var Bridge528_B = { };
+            Bridge.define("Test.BridgeIssues.N528.Bridge528_B", {
+                $scope: Bridge528_B
+            });
+        return Bridge528_B;
+    });
 
-define("Bridge528_C", ["bridge","dep1"], function (_, dep1) {
-    var exports = { };
-        Bridge.define("Test.BridgeIssues.N528.Bridge528_C", {
-            $scope: exports
-        });
-    return exports;
-});
+    define("Bridge528_C", ["dep1"], function (dep1) {
+        var Bridge528_C = { };
+            Bridge.define("Test.BridgeIssues.N528.Bridge528_C", {
+                $scope: Bridge528_C
+            });
+        return Bridge528_C;
+    });
 
 });
