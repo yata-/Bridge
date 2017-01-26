7578b372344c538a78a045b3370fb254edab531c
LeonidVeriga <leonid@object.net>
2017-01-26 15:37:04 +0300

    [2217] Compiler Test 07 to outputBy project

--- Compiler/TranslatorTests/TestProjects/07/Bridge/reference/test.js
+++ Compiler/TranslatorTests/TestProjects/07/Bridge/reference/test.js
@@ -72,6 +72,83 @@ Bridge.assembly("TestProject", function ($asm, globals) {
             }
         }
     });
+
+    /** @namespace TestProject1 */
+
+    /**
+     * @class TestProject1.TestClassA
+     */
+    Bridge.define("TestProject1.TestClassA", {
+        config: {
+            properties: {
+                /**
+                 * Some property
+                 *
+                 * @instance
+                 * @public
+                 * @this TestProject1.TestClassA
+                 * @memberof TestProject1.TestClassA
+                 * @function getValue1
+                 * @return  {number}
+                 */
+                /**
+                 * Some property
+                 *
+                 * @instance
+                 * @public
+                 * @this TestProject1.TestClassA
+                 * @memberof TestProject1.TestClassA
+                 * @function setValue1
+                 * @param   {number}    value
+                 * @return  {void}
+                 */
+                Value1: 0
+            }
+        },
+        /**
+         * GetMyValue method
+         *
+         * @instance
+         * @public
+         * @this TestProject1.TestClassA
+         * @memberof TestProject1.TestClassA
+         * @param   {number}    i    Number of somethng
+         * @return  {string}         A good string
+         */
+        getMyValue: function (i) {
+            return "";
+        }
+    });
+
+    /** @namespace TestProject2 */
+
+    /**
+     * @class TestProject2.TestClassB
+     */
+    Bridge.define("TestProject2.TestClassB", {
+        config: {
+            properties: {
+                /**
+                 * @instance
+                 * @public
+                 * @this TestProject2.TestClassB
+                 * @memberof TestProject2.TestClassB
+                 * @function getValue1
+                 * @return  {number}
+                 */
+                /**
+                 * @instance
+                 * @public
+                 * @this TestProject2.TestClassB
+                 * @memberof TestProject2.TestClassB
+                 * @function setValue1
+                 * @param   {number}    value
+                 * @return  {void}
+                 */
+                Value1: 0
+            }
+        }
+    });
         return MyModule;
     });
 
